a7356f4072f40c0d277f666f144c19f67f5b0786
challenger <challenger>
2002-06-28 13:54:26 +0000

    fixes

--- src/PCP/Type.hs
+++ src/PCP/Type.hs
@@ -4,21 +4,27 @@
 -- mai99byv@studserv.uni-leipzig.de
 
 module PCP.Type (
-	  module Iso
+      module Iso
+    , module Number
     , PCP (..)
     , module ToDoc
-	)where
+    )where
 
 import ToDoc
 import Iso
+import Number
 
-data PCP a = PCP [(a,(String, String))] deriving (Show,Read)
+data PCP = PCP [(Int,(String, String))] deriving (Show,Read)
 
 -- ToDoc Instanz
-instance (Show a) => ToDoc (PCP a) where
+instance ToDoc PCP where
 	toDoc (PCP pcp) = text "PCP" <+> text (show pcp)
 
 -- Isomorphie Instanz
 -- !!!!!!!!!!!!Muss noch fertig gestellt werden
-instance (Eq a, Ord a) => Iso (PCP a) where
+instance Iso (PCP) where
 	iso pcp1 pcp2 = False
+
+
+instance Number PCP PCP where
+   number = id
