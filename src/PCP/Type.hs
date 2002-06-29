55a962503330e4c2eeb9c082c9d3c3096e7bc137
Johannes Waldmann <joe>
2002-06-29 15:45:05 +0000

    PCP fix

--- src/PCP/Type.hs
+++ src/PCP/Type.hs
@@ -3,22 +3,24 @@
 -- autor Markus Kreuz
 -- mai99byv@studserv.uni-leipzig.de
 
+-- patches joe@informatik.uni-leipzig.de
+
 module PCP.Type (
       module Iso
     , module Number
     , PCP (..)
     , module ToDoc
-    )where
+    ) where
 
 import ToDoc
 import Iso
 import Number
 
-data PCP = PCP [(Int,(String, String))] deriving (Show,Read)
+data PCP = PCP [(String, String)] deriving (Show,Read)
 
 -- ToDoc Instanz
 instance ToDoc PCP where
-	toDoc (PCP pcp) = text "PCP" <+> text (show pcp)
+	toDoc (PCP pcp) = text "PCP" <+> toDoc pcp
 
 -- Isomorphie Instanz
 -- !!!!!!!!!!!!Muss noch fertig gestellt werden
