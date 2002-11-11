3bb297b1e897974f998b0152692b7b24f6878cc8
Johannes Waldmann <joe>
2002-11-11 23:05:08 +0000

    stuff vergessen

---
+++ src/Language/Potenzen.hs
@@ -0,0 +1,57 @@
+module Language.Potenzen
+
+-- $Id$
+
+where
+
+import Language.Type
+import Language.Zahl
+
+import Set
+import Random
+import Monad ( guard )
+
+alpha = [ '0' .. '9' ]
+
+wurzel :: Int -> Integer -> Integer
+-- bestimmt abgerundete wurzel
+wurzel e n | n <  0 = error "Language.Potenzen.wurzel: radikand <  0"
+wurzel 1 n = n
+wurzel e 0 = 0
+wurzel e 1 = 1
+wurzel e n | e <= 0 = error "Language.Potenzen.wurzel: exponent <= 0"
+wurzel e n = 
+    let c = 1
+	ee = fromIntegral e
+	f :: Integer -> Integer
+	f x = (n + (ee-1) * x ^ e) `div` ( ee * x ^ (ee-1) )
+	settle (x : y : ys) = if abs (x - y) <= 1 then x else settle (y : ys)
+	d = settle $ iterate f c
+    in	case ( do x <- reverse [ d - 1  .. d + 1 ]
+		  let y = x ^ e
+		  guard $ y <= n
+		  return x )
+	   of x : _ -> x
+	      _	    -> error $ "wurzel" ++ show (e, n, c, d)
+
+
+ist_wurzel :: Int -> Integer -> Bool
+ist_wurzel e n = n == (wurzel e n) ^ e
+
+potenzen :: Int -> Language
+potenzen e = Language
+	    { abbreviation = "{ dezimal(n^" ++ show e ++ ") : n >= 0 }"
+	    , alphabet	   = mkSet alpha
+	    , contains	   = \ w -> 
+	          all isDigit w && ( ist_wurzel e (read w :: Integer) )
+	    , sample       = \ c n -> sequence $ replicate c $ sam e n
+	    }
+
+sam :: Int -> Int -> IO String
+-- würfelt ein Wort von ungefähr passender Länge
+sam e l = do
+    w <- zahl l
+    let n = read w
+    let x = wurzel e n
+    return $ show $ x ^ e
+
---
+++ src/Language/Pump_Counter.hs
@@ -0,0 +1,39 @@
+module Language.Pump_Counter 
+
+-- $Id$
+
+where
+
+import Language.Type
+
+import Set
+import Random
+
+alpha = "abc"
+
+pump_counter :: Language
+pump_counter = Language
+	    { abbreviation = "{ a^x b^y c^z : x = 0 oder y = z }"
+	    , alphabet	   = mkSet alpha
+	    , contains	   = \ w -> 
+	          let ( aa, bcs ) = span ( == 'a' ) w
+		      ( bb, cs  ) = span ( == 'b' ) bcs
+		      ( cc, rest) = span ( == 'c' ) cs
+		  in     null rest
+		      && ( length aa == 0 || length bb == length cc )
+	    , sample       = \ c n -> sequence $ replicate c $ sam n
+	    }
+
+sam :: Int -> IO String
+-- würfelt ein Wort von ungefähr passender Länge
+sam l = do
+    f <- randomRIO (False, True)
+    let form x y z =  replicate x 'a' ++ replicate y 'b' ++ replicate z 'c'
+    if f -- set x = 0
+       then do y <- randomRIO (0, l)
+	       return $ form 0 y (l - y)
+       else do y <- randomRIO (0, l `div` 2)
+	       let x = l - 2 * y
+	       return $ form x y y
+
+
