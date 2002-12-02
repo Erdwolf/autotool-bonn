d630e44c1fac592569b24812b61fe22f7eb29c44
Johannes Waldmann <joe>
2002-12-02 14:03:27 +0000

    plus Meta

---
+++ src/Shift/Meta.hs
@@ -0,0 +1,79 @@
+module Shift.Meta 
+
+( Meta (..)
+, meta
+)
+
+where
+
+-- $Id$
+
+import Util.Size
+import ToDoc
+
+import Shift.Type
+import Shift.Verify
+
+import Shift.Computer (find, zustands_folge)
+
+import Reporter
+
+vector :: Meta -> Int -> Pins
+vector me k = do
+    (x, d) <- zip (start me) (diff me)
+    return $ x + k * d
+
+meta :: Int -> Meta -> Reporter Int 
+meta limit me = do
+    inform $ text "Sie haben eingesandt:" <+> toDoc me
+    newline
+
+    let shs0 = do 
+          (k, (q,p)) <- zip [0..] $ qps me
+	  return $ Shift { pins = vector me k
+			 , vorperiode = q
+			 , periode = p
+			 }
+    -- jetzt abschneiden
+    let shs = take 10 
+	    $ takeWhile ( \ sh -> periode sh <= limit ) 
+	    $ shs0
+    
+    inform $ text "Ich pr¸fe die Shift-Instanzen"
+    inform $ toDoc shs
+    sequence_ $ map (silent . verify limit) shs
+    inform $ text "OK"
+    newline    
+
+    let ps = take 20 $ map periode shs0
+    inform $ text "Ich untersuche das Wachstum der Periodenl‰ngen."
+
+    delta 0 ps
+
+
+delta :: Int -> [Int] -> Reporter Int
+delta deg ps = do
+    inform $ fsep [ text "Betrachte die", toDoc deg <> text "-te"
+		  , text "Differenzenfolge", toDoc ps
+		  ]
+    if ( null ps )
+       then do
+	 inform $ text "Die Folge ist zu kurz,"
+		    <+> text "ich kann das Wachstum nicht bestimmen."
+	 inform $ text "Da bis hier alle Differenzen >= 0 waren, nehme ich an,"
+	 inform $ text "daﬂ die Folge wenigstens wie ein Polynom"
+	 inform $ text "vom Grad" <+> toDoc (deg - 1) <+> text "w‰chst"
+	 return $ deg - 1
+       else do
+
+    if ( all (== 0) ps )
+       then do
+	    inform $ text "Diese Folge ist konstant 0,"
+	    inform $ text "deswegen ist die Original-Folge wahrscheinlich"
+	    inform $ text "ein Polynom vom Grad" <+> toDoc (deg-1)
+	    return (deg-1)
+       else do
+	    when ( any ( < 0 ) ps )
+		 $ reject $ text "Einige Elemente sind < 0"
+	    delta (deg + 1) $ zipWith (-) (tail ps) ps
+
