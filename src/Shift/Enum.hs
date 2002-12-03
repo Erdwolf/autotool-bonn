09943906bde3a7c0d5d27c7ca62523cd94fef04f
Johannes Waldmann <joe>
2002-12-03 16:12:30 +0000

    enum

--- src/Shift/Enum.hs
+++ src/Shift/Enum.hs
@@ -1,8 +1,12 @@
 module Shift.Enum where
 
+import List ( tails )
+import Monad ( guard )
+
 subsets :: Int -> [a] -> [[a]]
-subsets k xs | k > length xs = []
-subsets 0 xs = [[]]
-subsets k (x : xs) 
-    =  subsets k xs
-    ++ do ys <- subsets (k-1) xs ; return $ x : ys
+subsets 0 xs = return []
+subsets s xs = do
+    guard $ not $ null $ drop (s-1) xs
+    y : rest <- tails xs
+    ys <- subsets (s-1) rest
+    return $ y : ys
