5d9b2ac742c26957b4c5d10c84b6951ccc4f8228
Johannes Waldmann <joe>
2003-01-28 18:48:08 +0000

    plus 3COL interactive

--- src/PCP/Type.hs
+++ src/PCP/Type.hs
@@ -12,21 +12,3 @@ module PCP.Type (
     , module ToDoc
     ) where
 
-import ToDoc
-import Iso
-import Number
-
-data PCP = PCP [(String, String)] deriving (Show,Read)
-
--- ToDoc Instanz
-instance ToDoc PCP where
-	toDoc (PCP pcp) = text "PCP" <+> toDoc pcp
-
--- Isomorphie Instanz
--- !!!!!!!!!!!!Muss noch fertig gestellt werden
-instance Iso (PCP) where
-	iso pcp1 pcp2 = False
-
-
-instance Number PCP PCP where
-   number = id
