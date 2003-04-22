f9c97ae2e8a2d18b711f2ab3ba9c408e51fb6875
Johannes Waldmann <joe>
2003-04-22 19:34:52 +0000

    include Turing-laufzeit

--- src/Inter/Check.hs
+++ src/Inter/Check.hs
@@ -1,10 +1,14 @@
 -- this is a main module
 
 import qualified SAT.SAT
+import qualified TM.Steps
+
 import Inter.Types
 import Inter.Variant
 
 variants :: [ Variant ]
 variants = [ Variant SAT.SAT.var ]
 
-main = interface variants
+main = do
+     steps <- TM.Steps.variantsIO 
+     interface $ steps ++ variants
