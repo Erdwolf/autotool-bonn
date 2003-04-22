d4abd7f88b5884942f88093a4a46d40eedb950ba
Alf Richter <alf>
2003-04-22 10:43:32 +0000

    - mglNextAufgabenDB nun mit DATE_FORMAT

--- src/control/SQLqueries.hs
+++ src/control/SQLqueries.hs
@@ -572,7 +572,10 @@ mglNextAufgabenDB snr = do
 	stat <- query conn
 			( concat
 			  [ "SELECT aufgabe.ANr, aufgabe.Name AS Typ, aufgabe.Subject AS Nr, aufgabe.Highscore \n"
-			  , ", aufgabe.Von, aufgabe.Bis \n"
+			  , ", DATE_FORMAT( aufgabe.Von , \"%H:%i %a %d. %b %Y\") as Von " 
+			  , ", DATE_FORMAT( aufgabe.Bis , \"%H:%i %a %d. %b %Y\") as Bis\n"
+--			  , ", aufgabe.Von " 
+--			  , ", aufgabe.Bis "
 			  , "FROM aufgabe "
 			  , if null snr 
 			    then " \n" 
