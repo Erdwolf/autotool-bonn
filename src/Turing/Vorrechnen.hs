c06af80bdedbad950cd900f5909f39b91f824941
Johannes Waldmann <joe>
2003-04-22 12:21:36 +0000

    mehr layout

--- src/Turing/Vorrechnen.hs
+++ src/Turing/Vorrechnen.hs
@@ -24,17 +24,17 @@ vorrechnen_cut :: TUM y z
 vorrechnen_cut cut a xs = do
     inform $ fsep [ text "mit Eingabe", toDoc xs
 		  , text "erreicht der Automat folgende Konfigurationen:"
-		  , parens (text "ich zeige maximal die ersten" 
+		  , nest 8 $ parens (text "ich zeige maximal die ersten" 
 			    <+> toDoc cut <+> text "Schritte")
 		  ]
-    inform $ vcat $ do
+    inform $ nest 4 $ vcat $ do
         (i, ks) <- zip [0 :: Int .. ] 
 		$ take cut
 		$ map setToList 
 		$ schichten ( folgekonfigurationen a )
 		$ start_konfiguration a xs
 	return  $ text "nach" <+> toDoc i <+> text "Schritten:"
-	        $$ ( vcat $ do k <- ks; return $ toDoc k )
+	        <+> ( vcat $ do k <- ks; return $ toDoc k )
 
     
 vorrechnens :: TUM y z
