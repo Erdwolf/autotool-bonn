49153517d3db2e05122ef554ada31a32b5597e83
Johannes Waldmann <joe>
2003-05-07 07:27:29 +0000

    check arities

--- src/RAM/Check.hs
+++ src/RAM/Check.hs
@@ -13,14 +13,31 @@ builtins :: Set Builtin -> Program -> Reporter ()
 builtins allowed p = do
     inform $ text "erlaubt sind diese Builtin-Prozeduren:"
 	   <+> toDoc allowed
-    let you = mkSet $ do
+    let used = do
 	    b @ ( Builtin {} ) <- flatten p
+	    return b
+    let you = mkSet $ do
+	    b <- used
 	    return $ name b
     inform $ text "Sie benutzen:" <+> toDoc you
     let wrong = minusSet you allowed
     assert ( isEmptySet wrong ) $ text "sind alle zugelassen?"
+    mapM_ check_arity used
 
-    -- todo: teste arity der builtins
+check_arity :: Statement -> Reporter ()
+check_arity b = do
+    let ( ar, fun ) = get $ name b
+    when ( length ( args b ) /= ar )
+	 $ reject $ vcat [ text "Fehler in Anweisung" <+> toDoc b 
+			 , fsep [ text "Das Builtin" , toDoc ( name b )
+				, text "benötigt genau" ,toDoc ar
+				, text "Argumente"
+				]
+			 , fsep [ text "aber Sie verwenden"
+				, toDoc $ length $ args b
+				]
+			 ]
+
 
 loopy :: Program -> Reporter ()
 loopy p = do
