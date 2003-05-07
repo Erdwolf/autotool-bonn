7d0073b1d0ae44ac9498afca4a253bee4338e262
Johannes Waldmann <joe>
2003-05-07 08:02:02 +0000

    clipping

--- src/RAM/State.hs
+++ src/RAM/State.hs
@@ -17,5 +17,5 @@ instance ToDoc State where
     toDoc s = text "State" <+> dutch_record 
 	    [ text "memory" <+> equals <+> toDoc ( memory s )
 	    , text "todo" <+> equals 
-	      <+> clipped_dutch_list 3 ( map toDoc $ todo s )
+	      <+> clipped_dutch_list 1 ( map toDoc $ todo s )
 	    ]
