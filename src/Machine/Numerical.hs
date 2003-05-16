eeffd6ec383c326e3a63838bdf2ce16ef2db2a64
Johannes Waldmann <joe>
2003-05-16 12:02:57 +0000

    file

---
+++ src/Machine/Numerical.hs
@@ -0,0 +1,12 @@
+module Machine.Numerical 
+
+( module Machine.Numerical.Type
+, module Machine.Numerical.Inter
+)
+
+where
+
+-- $Id$
+
+import Machine.Numerical.Type
+import Machine.Numerical.Inter
