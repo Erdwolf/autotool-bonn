ab53a30964af5744d0684b6d09f95908c3f6eb7a
Johannes Waldmann <joe>
2003-05-16 16:45:16 +0000

    Acceptor

---
+++ src/Machine/Acceptor/Inter.hs
@@ -0,0 +1,44 @@
+module Machine.Acceptor.Inter where
+
+-- $Id$
+
+import qualified Machine.Acceptor.Type as A
+import Machine.Akzeptieren
+import Machine.Class
+
+import qualified Challenger as C
+
+import Inter.Types
+import Reporter hiding ( output )
+import ToDoc
+import Informed
+
+acceptor :: ( Machine m dat conf, ToDoc [dat] )
+         => String		-- aufgabe (major)
+	 -> String -- version ( minor )
+     -> A.Type m dat 
+     -> Var A.Acceptor ( A.Type m dat ) m
+acceptor auf ver num =
+    Var { problem = A.Acceptor
+	, aufgabe = auf
+	, version = ver
+	, key = \ matrikel -> do
+	      return matrikel
+	, gen = \ matrikel -> do
+	      inform $ text "Konstruieren Sie eine Maschine,"
+	      inform $ text "welche die Sprache" <+> info num 
+	               <+> text "akzeptiert!"
+	      return num
+	, gen_i = \ matrikel -> num
+	}
+
+{-
+instance ( Machine m dat conf , ToDoc [dat] ) 
+         => C.Partial A.Acceptor ( A.Type m dat conf ) m where
+    initial p i   = A.start i
+    partial p i b = A.check i b
+    total   p i b = do
+        positiv_liste (A.cut i) b $ A.yeah i
+        negativ_liste (A.cut i) b $ A.noh  i
+        return () -- größe der maschine (hier) ignorieren
+-}
