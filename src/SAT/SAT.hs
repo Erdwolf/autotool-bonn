04d555672cc463ff6e54ac68f5de8ce6a17f8d02
Johannes Waldmann <joe>
2003-06-27 12:48:12 +0000

    drifting

--- src/SAT/SAT.hs
+++ src/SAT/SAT.hs
@@ -23,14 +23,16 @@ module SAT.SAT
 , Variable, Literal (..)
 , Klausel, Formel, Belegung
 , module FiniteMap
-, var
+
 )
 
 where
 
 import FiniteMap
 import ReadFM
-import Challenger
+
+import Challenger.Partial
+
 import ToDoc
 import Monad (guard)
 import Sets
@@ -56,55 +58,13 @@ variablen f = mkSet $ do
     return $ case l of Pos v -> v ; Neg v -> v
 
 
-instance Problem SAT Formel Belegung where 
-
- 
-   validiere SAT f b = 
-       let vf = variablen f
-	   vb = mkSet (keysFM b)
-           fb = minusSet vf vb
-	   bf = minusSet vb vf
-       in
-	if not ( isEmptySet fb )
-	then ( False, text "Diese Variablen sind nicht belegt:" <+> toDoc fb )
-	else if not ( isEmptySet bf )
-	then ( False, text "Diese Variablen sind nicht in der Formel:" <+> toDoc bf )
-	else (True, text "Die Belegung passt zur Formel.")
-
-
-   verifiziereR SAT f b = do
-        partial SAT  f b
-	total   SAT f b
-
-
--- Erzeugt HTML-File zur Visualisierung
-   getInstanz SAT f b dateiName =
-	do 
-	 writeFile (dateiName ++ ".html")
-	            ("<br><table borders><caption>Diese Formael ist zu erfüllen:</caption>" ++ (erzInstanz f) ++ "</table>")
-	 return (dateiName ++ ".html", "html",ExitSuccess)
-
-
--- Erzeugt HTML-File zur Visualisierung
-   getBeweis SAT f b dateiName =
-	do
-	 writeFile (dateiName ++ ".html")
-		    (erzBeweis b)
-	 return (dateiName ++ ".html", "html",ExitSuccess)
-
-
-instance Number Formel Formel where number = id 
-
--- Version 1
-instance Iso Formel where iso f1 f2 = (mkSet f1) == (mkSet f2)
-
-
 ---------------------------------------------------------------------------
 
 instance Partial SAT Formel Belegung where
 
-
-    initial SAT g = emptyFM
+    initial SAT f = 
+        let v : vs = setToList $ variablen f
+	in  listToFM [ ( v, True ) ]
 
     partial SAT f b = do
 
@@ -137,99 +97,5 @@ instance Partial SAT Formel Belegung where
 
 
 
-instance  Step SAT Formel Belegung ( Paint String (Maybe Bool) ) where
-        step SAT f b ( Paint v mc ) =
-	    case mc of
-	         Nothing -> delFromFM b v
-		 Just c  -> addToFM   b v c
-
-instance  Interactive SAT Formel Belegung ( Paint String (Maybe Bool) ) where
-
-  interactive SAT f  = do
-
-    let doc = ( C.mkLabel "rechte Maustaste ergibt Popup-Menu" )
-	      { C.ident = C.Ident "doc" }
-
-    let handle v = \ ( C.Input cs) -> Paint v $
-           case reads cs of
-	        [(mc, "")] -> mc
-		_          -> Nothing
-
-    let result listener = 
-          let vccs = do 
-		 v <- setToList $ variablen f
-		 let c0 = ( C.mkLabel v ) 
-			  { C.ident = C.Ident $ v ++ "0" 
-			  }
-		 let c1 = ( C.mkChoice $ map show 
-			               $ Nothing : map Just [ False, True ] ) 
-			  { C.ident = C.Ident $ v ++ "1"
-			  , C.action = C.translate ( handle v ) listener
-			  }
-		 return (v, c0, c1)
-
-              set = C.splits $ do
-			(v, c0, c1) <- vccs
-			return $ C.translate ( \ f -> 
-				      [ C.Text $ show $ lookupFM f v ] )
-			       $ C.changeL c1 
-          in ( C.column [ doc
-			, C.grid $ do 
-			     ( v, c0, c1 ) <- vccs
-			     return [ c0, c1 ]
-			]
-	     , set )
 
 
-    return $ result
-
-
-
-
-----------------------------------------------------------------------------
-
-
--- erzeugt den Ausgabestring fuer die HTML Ausgabe der Instanz
-erzInstanz :: Formel -> String
-erzInstanz f = "<tr><td>" ++ show f ++ "</td></tr>"
-
--- erzeugt den Ausgabestring fuer die HTML Ausgabe des Beweises
-erzBeweis :: Belegung -> String
-erzBeweis b = "<tr><td>" ++ show b ++ "</td></tr>"
-
-
-wert_formel :: Formel -> Belegung -> Bool
-wert_formel f b =
-    and [ wert_klausel k b | k <- f ]
-
-wert_klausel 	:: Klausel -> Belegung -> Bool
-wert_klausel (l1,l2,l3) b = wert_literal l1 b || wert_literal l2 b || wert_literal l3 b
-
-wert_literal 	:: Literal -> Belegung -> Bool
-wert_literal  (Pos v) b = wert_variable v b
-wert_literal  (Neg v) b = not (wert_variable v b)
-			
-wert_variable 	:: Variable -> Belegung -> Bool
-wert_variable v b =
-    case lookupFM b v of
-	Nothing -> error "variable nicht in belegung."
-	Just w  -> w
-
------------------------------------------------------------
-
-m_wert_formel :: Formel -> Belegung -> Maybe Bool
-m_wert_formel f b = do
-    ws <- mapM (flip m_wert_klausel b) f
-    return $ and ws
-
-m_wert_klausel :: Klausel -> Belegung -> Maybe Bool
-m_wert_klausel (l1, l2, l3) b = do
-    ws <- mapM (flip m_wert_literal b) [l1, l2, l3]
-    return $ or ws
-
-m_wert_literal 	:: Literal -> Belegung -> Maybe Bool
-m_wert_literal (Pos v) b = m_wert_variable v b
-m_wert_literal (Neg v) b = fmap not $ m_wert_variable v b
-
-m_wert_variable 	:: Variable -> Belegung -> Maybe Bool
-m_wert_variable v b = lookupFM b v
