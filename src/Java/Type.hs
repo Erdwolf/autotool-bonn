0051346e8f528c37bf49cfcb8fa9d29e3f766ba4
Johannes Waldmann <joe>
2003-10-16 12:44:24 +0000

    updates

--- src/Java/Type.hs
+++ src/Java/Type.hs
@@ -7,16 +7,16 @@ import ToDoc
 data Java = Java [ Statement ]
 
 data Statement = Block  [ Statement ]
-	       | Assign Expression Expression
+	       | Exp Expression
 	       | While  Expression Statement
 	       | If     Expression Statement Statement
-	       | Declare String String
+	       | Declare_Int Expression
 	       | Return Expression
 
 data Expression = Number Integer
 		| Name String
 		| Operation String Expression Expression
-		| Paren Expression
+		| Parens Expression
 
 ---------------------------------------------------------------------------
 
@@ -29,12 +29,13 @@ instance Show Statement where show = render . toDoc
 
 instance ToDoc Statement where
     toDoc ( Block sts ) = braces $ vcat $ map toDoc sts
-    toDoc ( While e st ) = text "while" <+> toDoc e <+> toDoc st
-    toDoc ( If e y n ) = text "if" <+> toDoc e <+> toDoc y
+    toDoc ( While e st ) = text "while" <+> parens (toDoc e) <+> toDoc st
+    toDoc ( If e y n ) = text "if" <+> parens (toDoc e) <+> toDoc y
 			     <+> text "else" <+> toDoc n
-    toDoc ( Assign l r ) = toDoc l <+> equals <+> toDoc r <+> semi
-    toDoc ( Declare ty na ) = text ty <+> text na <+> semi
-    toDoc ( Return e ) = text "return" <+> toDoc e <+> semi
+
+    toDoc ( Exp e ) = toDoc e <+> semi
+    toDoc ( Declare_Int e ) = text "int" <+> toDoc e <> semi
+    toDoc ( Return e ) = text "return" <+> toDoc e <> semi
 
 instance Show Expression where show = render . toDoc
 
@@ -42,18 +43,20 @@ instance ToDoc Expression where
     toDoc ( Number i ) = toDoc i
     toDoc ( Name n ) = text n
     toDoc ( Operation op l r ) = toDoc l <+> text op <+> toDoc r
-    toDoc ( Paren e ) = parens $ toDoc e
+    toDoc ( Parens e ) = parens $ toDoc e
 
 ---------------------------------------------------------------------------
 
+assign l r = Exp $ Operation "=" l r
+
 example :: Java
 example = Java 
-	[ Declare "int" "a"
-	, Assign ( Name "a" ) ( Number 0 )
-	, While ( Paren $ Operation ">" ( Name "x0" ) ( Number 0 ) )
-	        $ Block [ Assign ( Name "x0" ) 
+	[ Declare_Int ( Name "a" )
+	, assign ( Name "a" ) ( Number 0 )
+	, While ( Operation ">" ( Name "x0" ) ( Number 0 ) )
+	        $ Block [ assign ( Name "x0" ) 
 			         ( Operation "-" ( Name "x0" ) ( Number 1 ) )
-			, Assign ( Name "a" ) 
+			, assign ( Name "a" ) 
 			         ( Operation "+" ( Name "a" ) ( Number 1 ) )
 			]
 	, Return ( Name "a" )
