358cd60e176041a5accbbf7635d945eb0b52aae7
Johannes Waldmann <joe>
2003-11-05 09:50:03 +0000

    imports zerlegt

--- src/HTWK.hs
+++ src/HTWK.hs
@@ -4,52 +4,15 @@ module Inter.Boiler where
 
 import Inter.Types
 
-
-import qualified JVM.TIMES
-
--- ws03/compilerbau:
-
-import qualified JVM.TIMES
-import qualified JVM.EXP1
-import qualified JVM.EXP2
-
-import qualified JVM.NUM32
-import qualified JVM.NUM42
-import qualified JVM.NUMAT
-
-import qualified Syntax.Synthese
-import qualified Syntax.Analyse
-
--- ws03/informatik:
-
-import qualified Sortier.Netz.Check
-import qualified PCP.Quiz
-import qualified Hanoi
-import qualified Java.Sort
+import qualified Inter.Compilerbau
+import qualified Inter.Informatik
 
 -- hier steht ALLES machbare drin
 -- in der datenbank steht dann, zu welcher zeit es erlaubt ist.
 
 boiler :: IO [ Variant ]
-boiler = sequence $
+boiler = sequence 
        -- der erste ist der default-wert!
+       $  Inter.Compilerbau.aufgaben
+       ++ Inter.Informatik.aufgaben
 
-          Syntax.Synthese.generates
-       ++ Syntax.Analyse.generates
-
-       ++ Sortier.Netz.Check.generates 
-       ++ PCP.Quiz.generates
-       ++ Hanoi.generates
-
-       ++ map (fmap Variant) 
-       [ JVM.TIMES.generate
-       , JVM.EXP1.generate
-       , JVM.EXP2.generate
-
-       , JVM.NUM32.generate
-       , JVM.NUM42.generate
-       , JVM.NUMAT.generate
-       ]
-
-
-	++ Java.Sort.generates
