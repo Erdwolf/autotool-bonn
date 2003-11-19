9d5da89799cc39183d429b7b73617c04bf9020c5
Johannes Waldmann <joe>
2003-11-19 08:41:09 +0000

    fehler beim cache-lesen wegfangen

--- src/Util/Cache.hs
+++ src/Util/Cache.hs
@@ -3,18 +3,24 @@ module Util.Cache where
 -- $Id$
 
 import Util.Datei
-
 import Util.Debug
+
+import Control.Monad ( when )
+import qualified Control.Exception 
 
 cache :: (Show a, Read a) => Datei -> IO a -> IO a
 -- falls schon vorhanden, dann lesen
 -- sonst erzeugen und schreiben
-cache d action = 
-    catch ( do debug $ "lies cache-file " ++ show d ++ " ... "
+cache d action = Control.Exception.catch
+          ( do debug $ "lies cache-file " ++ show d ++ " ... "
 	       cs <- lesen d
 	       debug $ "OK"
                let a = read cs
-	       a `seq` return a -- ??
+	       -- the following forces evaluation of a ?
+	       when ( 0 == length ( show a )) $ do
+	            let msg = "cannot parse cache contents"
+	            debug msg ; fail msg
+	       return a 
 	  ) 
 	  ( \ _ -> do 
 		debug $ "nicht vorhanden, schreibe cache ... "
