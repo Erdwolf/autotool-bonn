69ce88f314ce14c5aba40685a9001395a3fb76c2
Johannes Waldmann <joe>
2004-04-14 07:51:02 +0000

    change comment sytax for haddock

--- src/Turing/Beaver.hs
+++ src/Turing/Beaver.hs
@@ -1,5 +1,5 @@
---| parser for busy beaver TMs, as in
---| <http://www.drb.insel.de/~heiner/BB/bb-list>
+-- | parser for busy beaver TMs, as in
+-- | <http://www.drb.insel.de/~heiner/BB/bb-list>
 
 module Turing.Beaver where
 
@@ -12,7 +12,7 @@ import qualified Turing.Dot
 import Control.Monad ( guard )
 
 
---| examples (Marxen/Buntrock), see above web site
+-- | examples (Marxen/Buntrock), see above web site
 five = " 1 B1L C1R C1L B1L D1L E0R A1R D1R H1L A0R  4098 47176870 "
 six  = " 3 B1R C0R A0L D0R D1R H1R E1L D0L F1R B1L A1R E1R "
 --    ones  = 2,537,699,363,594,175,843,063
