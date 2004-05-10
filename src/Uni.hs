423d8cf241d4cff3af4d77412a7cfa365735bcbd
Johannes Waldmann <joe>
2004-05-10 17:05:38 +0000

    moved boiler function. es soll so wenig wie moglich in uni/ stehen, weil die benutzung von pfaden (-i) uncool ist. richtig sind qualifizierte imports (Uni.*)

--- src/Uni.hs
+++ src/Uni.hs
@@ -2,28 +2,7 @@ module Inter.Boiler where
 
 --   $Id$
 
-import Inter.Types
-
-
--- import qualified Syntax.Analyse
--- import qualified Syntax.Synthese
--- import qualified Syntax.Grammatik
--- import qualified Syntax.Pumping
-
--- import Serie6
-import qualified Uni.SS04.Serie1
-import qualified Uni.SS04.Serie2
-
--- hier steht ALLES machbare drin
--- in der datenbank steht dann, zu welcher zeit es erlaubt ist.
-
-boiler :: IO [ Variant ]
-boiler = sequence -- der erste ist der default-wert!       
-       $  []
-
---       ++ Uni.SS04.Serie1.generate
-       ++ Uni.SS04.Serie2.generate
-
+import qualified Uni.SS04
 
 
 
---
+++ src/Uni/SS04.hs
@@ -0,0 +1,22 @@
+module Uni.SS04 where
+
+--  $Id$
+
+import Inter.Types
+
+import qualified Uni.SS04.Serie1
+import qualified Uni.SS04.Serie2
+import qualified Uni.SS04.Serie3
+
+
+-- hier steht ALLES machbare drin
+-- in der datenbank steht dann, zu welcher zeit es erlaubt ist.
+
+boiler :: IO [ Variant ]
+boiler = sequence -- der erste ist der default-wert!       
+       $  []
+
+--       ++ Uni.SS04.Serie1.generate
+       ++ Uni.SS04.Serie2.generate
+       ++ Uni.SS04.Serie3.generate
+
