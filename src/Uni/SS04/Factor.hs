9a8db3448bcbcea9a57eec22620dba533d04a2ea
Johannes Waldmann <joe>
2004-07-07 10:12:44 +0000

    cut wieder hoch

--- src/Uni/SS04/Factor.hs
+++ src/Uni/SS04/Factor.hs
@@ -76,6 +76,25 @@ generate = do
     return $ I.acceptor "TM" "FACTOR" it
 
 
+demo :: Turing Char Int
+demo = Turing { eingabealphabet = mkSet "1", arbeitsalphabet = mkSet "01"
+       , leerzeichen = '0', zustandsmenge = mkSet [ 0, 1, 2, 3, 4, 7 ]
+       , tafel = listToFM [ ( ( '0', 0 ), mkSet [ ( '1', 1, L ) ] )
+			  , ( ( '0', 1 ), mkSet [ ( '1', 2, L ) ] )
+			  , ( ( '0', 2 ), mkSet [ ( '1', 3, L ) ] )
+			  , ( ( '0', 3 ), mkSet [ ( '1', 0, R ), ('0', 4, L) ] )
+			  , ( ( '0', 4 ), mkSet [ ( '1', 7, L ) ] )
+			  , ( ( '1', 0 ), mkSet [ ( '1', 2, R ) ] )
+			  , ( ( '1', 1 ), mkSet [ ( '1', 1, L ) ] )
+			  , ( ( '1', 2 ), mkSet [ ( '0', 4, R ) ] )
+			  , ( ( '1', 3 ), mkSet [ ( '1', 3, R ) ] )
+			  , ( ( '1', 4 ), mkSet [ ( '0', 0, R ) ] )
+			  ]
+       , startzustand = 0, endzustandsmenge = mkSet [ 7 ]
+       }
+
+
+
 
 
 
