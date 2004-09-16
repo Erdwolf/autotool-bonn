2bde55e0ef920c5a6651cb953602150d707e5315
Johannes Waldmann <joe>
2004-09-16 08:37:10 +0000

    better display (of border path)

--- src/PCP/Paths.hs
+++ src/PCP/Paths.hs
@@ -15,16 +15,22 @@ import Autolib.Schichten
 import Autolib.Util.Sort
 import Autolib.Util.Size
 import Autolib.ToDoc
+import Autolib.Reader
+import Autolib.Hash
+
 
 import PCP.Type
 import PCP.Examples
 
 import Control.Monad.State
 import Control.Monad.Identity
+import Data.Maybe
+import Data.FiniteMap
 
 -- | check dis
 
 test = check simple $ take 4 ssimple
+emit = writeFile "vier.dot" $ show $ toDot $ automate $ check vier svier
 
 check pcp (k : ks) = runIdentity
       $ evalStateT ( do
@@ -44,10 +50,11 @@ data Config c a = Config
 		{ separator :: c
 		, pcp :: PCP c
 		, store :: [(a, c, a)]
-		, supply :: [a] -- lazy infinite
+		, supply :: [a] -- ^ lazy infinite
 		, used :: [a]
 		, from :: a, to :: a
 		, border :: [(a, c, a)]
+		, glue :: [(a, a)] -- ^ states should be identified
 		, cut :: a
 		, time :: Int
 		}
@@ -77,6 +84,7 @@ spiral k = do
     p <- gets pcp
     let (l, r) = p !! k
 
+    s <- gets separator
     bore <- gets border
 
     c <- gets cut
@@ -84,6 +92,12 @@ spiral k = do
        then do -- start next level
             jack <- next
             put_cut $ jack
+            add_glue (jack, end bore)
+
+            when ( c == begin bore ) $ do
+	        link (c, [s], jack)
+                return ()
+
             return jack
        else do -- continue old level
 	    return $ end bore
@@ -95,7 +109,6 @@ spiral k = do
     -- assert $ strip pre == r
     put_border post
         
-    s <- gets separator
     link (end pre, [s], end ext)	
     return ()
 
@@ -113,6 +126,7 @@ blank s p = Config { separator = s, pcp = p
 	       -- questionable
 	       , from = 0 , to = 0, cut = 0
 	       , border = []
+	       , glue = []
 	       , time = 0
 	       }
 
@@ -149,6 +163,10 @@ link (p, w, q) = do
     let pqs = p : this ++ [q]
     let path = zip3 pqs w (tail pqs)
     add_store path 
+
+add_glue g = do
+    conf <- get
+    put $ conf { glue = g : glue conf }
 
 add_store :: Monad m
 	  => [(a, c, a)] 
@@ -273,28 +291,56 @@ applicables srs w = do
 
 --------------------------------------------------------------------------
 
-data Net c = Net { net :: NFA c Int }
+data Net c a = Net { net :: NFA c (Label a) }
 
-instance NFAC c Int => ToDoc (Net c) where
+instance NFAC c a => ToDoc (Net c a) where
     toDoc = toDoc . net
-instance NFAC c Int => Show  (Net c) where
+instance NFAC c a => Show  (Net c a) where
     show = render . toDoc
-instance NFAC c Int => ToDot (Net c) where
+instance NFAC c a => ToDot (Net c a) where
     toDot = toDot . net
     toDotProgram _ = "neato"
-    toDotOptions _ = "-Gsize=8,11 -Gratio=fill" 
+    toDotOptions _ = unwords 
+         $ [ "-Nshape=point",  "-Nfixedsize=true"
+	   -- , "-Gsize=9,14", "-Gratio=fill"
+           , "-Gstart=10"
+	   ]
 
+data Label a = Label { it :: a, tag :: Doc }
+
+instance Eq a => Eq (Label a) where
+    x == y = it x == it y
+instance Ord a => Ord (Label a) where 
+    compare x y = compare (it x) (it y)
+instance Hash a => Hash (Label a) where
+    hash = hash . it
+
+instance ToDoc (Label a) where toDoc = tag
+instance Reader (Label a) -- dummy
+instance Show (Label a) where show = render . toDoc
+
+{-
 automate :: (  NFAC c Int )
     => Config c Int 
-    -> Net (RX c)
+    -> Net (RX c) (Label Int)
+-}
 automate conf = 
     let trs = store conf
+        fm = addListToFM_C (error "PCP.Paths.automate") emptyFM $ do
+                 (k, (x, y)) <- zip [0 :: Int ..] $ glue conf
+                 [ (x, toDoc k), (y, toDoc k) ]
+        label q = Label
+		{ it = q
+		, tag = lookupWithDefaultFM fm empty q
+		}
     in  Net $ Autolib.NFA.Compact.make
+            $ statemap label
 	    $ NFA { nfa_info = funni "automate" [ ] 
 		 , states = mkSet $ used conf
 		 , alphabet = mkSet $ strip trs
 		 , starts = unitSet $ from conf
-		 , finals = unitSet $ to conf
+                 -- so that all glue points remain visible when compacting
+		 , finals = mkSet $ keysFM fm
 		 , trans  = collect trs
 		 }
 
