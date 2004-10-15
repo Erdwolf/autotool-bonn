7df116da1702a84df0a75f9df2d5a66c2e9cb77a
Johannes Waldmann <joe>
2004-10-15 08:18:07 +0000

    CGI library verbessert (env wird durchgereicht)

--- src/Inter/Super.hs
+++ src/Inter/Super.hs
@@ -42,7 +42,7 @@ iface :: [ Make ] -> Form IO ()
 iface mks = do
 
     vors <- io $ Control.Vorlesung.get
-    vnr <- require $ selector "vnr" "wähle Vorlesung" 0 $ do
+    vnr <- req_selector_submit "vnr" "wähle Vorlesung" 0 $ do
         vor <- vors
         return ( show $ Control.Vorlesung.name vor
 	       , Control.Vorlesung.vnr vor 
@@ -52,51 +52,59 @@ iface mks = do
     -- sondern nur die mit passender VNr
     aufs <- io $ Control.Aufgabe.get  
     br
-    manr  <- require $ selector "anr" "bearbeite Aufgabe" 0 
+    manr <- req_selector_submit "anr" "bearbeite Aufgabe" 0 
       $ ( "(neue Aufgabe)", Nothing ) : do
         auf <- aufs
         guard $ Control.Aufgabe.vnr auf == vnr
         return ( show $ Control.Aufgabe.name auf
 	       , Just $ Control.Aufgabe.anr auf
 	       )
-    submit "sauf" "edit"
 
     pre $ "Vorlesung gewählt : " ++ show vnr
     pre $ "Aufgabe gewählt : " ++ show manr
 
-    mk <- require $ selector "mk" "wähle Aufgabentyp" 0 $ do
+    mk <- req_selector_submit "mk" "wähle Aufgabentyp" 0 $ do
         mk <- mks
 	return ( show mk, mk )
 
     case mk of 
         Make doc fun ex -> do
-            ( mconf , sconf ) <- editor_submit "conf" "Konfiguration" ex
-            case mconf of
-                 Nothing -> return ()
-                 Just conf -> do
-                      let var :: Var p i b = fun conf
-                          p = problem var
-                      m0 <- io $ randomRIO (0, 999999 :: Int) 
-                      mat <- with ( show m0 ) $ textfield "mat" ( show m0 )
-                      k <- io $ key var mat -- some key
-                      g <- io $ gen var k
-                      let ( Just i , com :: Doc ) = export g
-                          desc = describe (problem var) i
-                          ini = initial (problem var) i
-                      h3 "Aufgabenstellung"
-                      pre $ show desc
-                      ( mb, sb ) <- editor_submit "b" "Lösung" ini
-                      case mb of
-			   Nothing -> return ()
-                           Just b -> do
-      	                       let (res, com :: Doc) = export $ evaluate' p i b
-                               pre $ show com
+            conf <- req_editor_submit "conf" "Konfiguration" ex
+            let var :: Var p i b = fun conf
+                p = problem var
+            br
+            m0 <- io $ randomRIO (0, 999999 :: Int) 
+            mat <- with ( show m0 ) $ textfield "mat" ( show m0 )
+            k <- io $ key var mat -- some key
+            g <- io $ gen var k
+            let ( Just i , com :: Doc ) = export g
+                desc = describe (problem var) i
+                ini = initial (problem var) i
+            h3 "Aufgabenstellung"
+            pre $ show desc
+            b <- req_editor_submit "b" "Lösung" ini
+     	    let (res, com :: Doc) = export $ evaluate' p i b
+            pre $ show com
 
+--------------------------------------------------------------------------
 
-editor_submit :: ( ToDoc a, Reader a, Monad m )
+req_selector_submit tag title def opts = do
+    mopt <- selector ("L" ++ tag) title def opts
+    sopt <- submit   ("S" ++ tag) "submit"
+    case mopt of
+	 Nothing -> mzero
+	 Just opt -> do
+	      when sopt blank
+	      return opt
+
+req_editor_submit :: ( ToDoc a, Reader a, Monad m )
 	      => Tag -> String -> a 
-	      -> Form m ( Maybe a , Bool )
-editor_submit tag title ex = do
+	      -> Form m a
+req_editor_submit tag title ex = do
     mconf <- editor ("E" ++ tag) title ex
     sconf <- submit ("S" ++ tag) "submit"
-    return ( mconf , sconf )
+    case mconf of
+	 Nothing -> mzero
+	 Just conf -> do
+             when sconf blank
+	     return conf
