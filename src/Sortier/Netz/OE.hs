faa92856d296976d2d4a7ce0907207ba0a59a21b
Johannes Waldmann <joe>
2004-11-04 20:19:42 +0000

    fix network

--- src/Sortier/Netz/OE.hs
+++ src/Sortier/Netz/OE.hs
@@ -11,6 +11,20 @@ sort xs =
 	    ++ sort top
             ++ merge bot top
 
+-- merge [x1, x2, .. xj ] [ y1, y2, .. yk ]
+-- = let z  = merge [ x1 x3 .. ] [ y1 y3 .. ]
+--       z' = merge [ x2 x4 .. ] [ y2 y4 .. ]
+--   in  z1 (swap z1' z2) (swap z2' z3) .. 
+
+-- merge        [1 3 6 7 9] [2 4 5 ]
+--   z  = merge [1   6   9] [2   5 ] = 1   2   5 6   9
+--   z' = merge [  3   7  ] [  4   ] =   3   4     7
+--     [ 1, 2 <-> 3, 5 <-> 4, 6 <-> 7, 9
+
+-- merge [ 1 5 ] [ 2 3 6 7 8 ]
+--  merge[ 1   ] [ 2   6   8 ] => [ 1    2   6   8 ]
+-- merge [   5 ] [   3   7   ] => [    3   5   7   ]
+
 merge :: [Int] -> [Int] -> [(Int,Int)]
 merge [] ys = []
 merge xs [] = []
@@ -20,7 +34,9 @@ merge xs ys =
 	(yodds, yevens) = halve ys
     in     merge xodds yodds
 	++ merge xevens yevens
-        ++ zip (xevens ++ yevens)  (tail xodds ++ yodds)
+        ++ if odd (length xs)
+	   then zip (xevens ++ yodds) (tail xodds ++ yevens)
+	   else zip (xevens ++ yevens) (tail xodds ++ yodds)
 
 halve [] = ([], [])
 halve (x : xs) = 
