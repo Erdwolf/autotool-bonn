a98f031171ba06d288430f675b05b79a03ea5014
Johannes Waldmann <joe>
2004-11-13 17:57:42 +0000

    file

---
+++ src/Control/Stud_Aufg.hs
@@ -0,0 +1,19 @@
+module Control.Stud_Aufg 
+
+--  $Id$
+
+( module Control.Stud_Aufg.Typ
+, module Control.Stud_Aufg.DB
+)
+
+where
+
+import Control.Stud_Aufg.Typ
+import Control.Stud_Aufg.DB
+
+
+
+
+
+
+
