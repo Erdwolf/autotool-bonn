4a50deff61cd9871dc1078c887647ccbfafc5dcc
Johannes Waldmann <joe>
2004-12-08 01:23:47 +0000

    files

---
+++ src/Exp/Convert.hs
@@ -0,0 +1,109 @@
+module Exp.Convert where
+
+--  $Id$
+
+import Convert.Type
+
+import Inter.Types
+import Inter.Quiz
+import Autolib.ToDoc
+import Autolib.Hash
+import Autolib.Util.Seed
+import qualified Challenger as C
+
+import Data.Typeable
+import Autolib.Reporter
+
+import Autolib.Exp.Example
+import Autolib.NFA.Eq
+import Autolib.Exp
+import Autolib.Exp.Inter
+import Autolib.Exp.Sanity
+import Autolib.Size
+import Autolib.Dot ( peng )
+
+import Exp.Property
+import Exp.Test
+import Exp.Quiz
+import NFA.Roll
+import Convert.Input
+
+import Autolib.Set
+import Autolib.Informed
+
+data Convert_To_Exp = Convert_To_Exp 
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+instance C.Partial Convert_To_Exp 
+                   ( Convert , [ Property Char ] ) 
+		   ( RX Char ) 
+  where
+
+    report Convert_To_Exp ( from, props ) = do
+        inform $ vcat
+            [ text "Gesucht ist ein regulärer Ausdruck,"
+	    , text "der die Sprache"
+	    , nest 4 $ form from
+	    , text "erzeugt und folgende Eigenschaften hat:"
+	    , nest 4 $ toDoc props
+            ]
+        case ( name from , input from ) of
+	     ( Nothing , NFA aut ) -> do
+                 inform $ text "Das ist der Automat:"
+		 peng aut
+             _ -> return ()
+
+    initial Convert_To_Exp ( from, props ) = 
+        let [ alpha ] = do Alphabet a <- props ; return a
+	in  Autolib.Exp.Example.example alpha
+
+    partial Convert_To_Exp ( from, props ) exp = do
+        sanity_keys ( mkSet [ "Empty", "All", "Sigma" ] ) exp
+        inform $ text "Sind alle Eigenschaften erfüllt?"
+        nested 4 $ mapM_ ( flip test exp ) props
+
+    total Convert_To_Exp ( from, props ) exp = do
+        inform $ text "Erzeugt Ihr Ausdruck die richtige Sprache?"
+        let [ alpha ] = do Alphabet a <- props ; return a
+        flag <- nested 4 
+             $ equ ( informed ( text "Sprache der Aufgabenstellung" ) $ eval alpha from ) 
+                   ( informed ( text "Sprache Ihres Ausdrucks" ) 
+                   $ inter (std_sigma $ setToList alpha) exp 
+		   )
+        when (not flag) $ reject $ text ""
+
+
+instance C.Measure Convert_To_Exp 
+                   ( Convert, [ Property Char ] ) 
+		   ( RX Char ) 
+  where
+    measure _ _ exp = fromIntegral $ size exp
+
+make :: Make
+make = direct Convert_To_Exp 
+     ( Convert { name = Just "Wörter über {a,b}, die mit  a  beginnen und mit  b  enden"
+	       , input = Exp $ read "a (a+b)^* b"
+	       }
+     , Exp.Property.example
+     )
+
+
+
+instance Generator Convert_To_Exp ( Quiz Char ) 
+           ( Convert, [ Property Char ] ) where
+    generator p quiz key = do
+        aut <- roll $ generate quiz
+        return ( Convert { name = Nothing
+		   , input = NFA aut 
+		   }
+	       , solve quiz
+	       )
+
+instance Project  Convert_To_Exp 
+                  ( Convert, [ Property Char ] ) 
+                  ( Convert, [ Property Char ] )  where
+    project p x = x
+
+
+qmake :: Make
+qmake = quiz Convert_To_Exp Exp.Quiz.example
---
+++ src/NFA/Convert.hs
@@ -0,0 +1,101 @@
+module NFA.Convert where
+
+--  $Id$
+
+import Convert.Type
+
+import Inter.Types
+import Inter.Quiz
+import Autolib.ToDoc
+import Autolib.Hash
+import Autolib.Util.Seed
+import qualified Challenger as C
+
+import Data.Typeable
+import Autolib.Reporter
+
+import Autolib.NFA ( NFA )
+import Autolib.NFA.Example
+import Autolib.NFA.Eq
+import Autolib.NFA.Restrict
+import Autolib.Size
+import Autolib.Dot ( peng )
+import Autolib.Informed
+
+import NFA.Property
+import NFA.Test
+import NFA.Quiz
+import Exp.Roll
+import Convert.Input
+
+data Convert_To_NFA = Convert_To_NFA 
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+instance C.Partial Convert_To_NFA 
+                   ( Convert , [ Property Char ] ) 
+		   ( NFA Char Int ) 
+  where
+
+    describe Convert_To_NFA ( from, props ) = vcat
+        [ text "Gesucht ist ein endlicher Automat,"
+	, text "der die Sprache"
+	, nest 4 $ form from
+	, text "akzeptiert und folgende Eigenschaften hat:"
+	, nest 4 $ toDoc props
+        ]
+
+    initial Convert_To_NFA ( from, props ) = 
+        let [ alpha ] = do Alphabet a <- props ; return a
+	in  Autolib.NFA.Example.example_sigma alpha
+
+    partial Convert_To_NFA ( from, props ) aut = do
+        let [ alpha ] = do Alphabet a <- props ; return a
+        restrict_states aut
+        restrict_alpha alpha aut
+        inform $ text "Das ist Ihr Automat:"
+	peng $ aut
+        inform $ text "Sind alle Eigenschaften erfüllt?"
+        nested 4 $ mapM_ ( flip test aut ) props
+
+    total Convert_To_NFA ( from, props ) aut = do
+        inform $ text "Akzeptiert der Automat die richtige Sprache?"
+        let [ alpha ] = do Alphabet a <- props ; return a
+        flag <- nested 4 
+             $ equ ( informed ( text "Sprache der Aufgabenstellung" ) $ eval alpha from ) 
+                   ( informed ( text "Sprache Ihres Automaten" ) aut )
+        when (not flag) $ reject $ text ""
+
+
+instance C.Measure Convert_To_NFA 
+                   ( Convert, [ Property Char ] ) 
+		   ( NFA Char Int ) 
+  where
+    measure _ _ aut = fromIntegral $ size aut
+
+make :: Make
+make = direct Convert_To_NFA 
+     ( Convert { name = Nothing
+	       , input = Exp $ read "a (a+b)^* b"
+	       }
+     , NFA.Property.example
+     )
+
+
+instance Generator Convert_To_NFA ( Quiz Char ) 
+           ( Convert, [ Property Char ] ) where
+    generator p quiz key = do
+        ( exp, aut ) <- roll $ generate quiz
+        return ( Convert { name = Nothing
+		   , input = Exp exp
+		   }
+	       , solve quiz
+	       )
+
+instance Project  Convert_To_NFA 
+                  ( Convert, [ Property Char ] ) 
+                  ( Convert, [ Property Char ] )  where
+    project p x = x
+
+
+qmake :: Make
+qmake = quiz Convert_To_NFA NFA.Quiz.example
