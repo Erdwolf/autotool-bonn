8faa32112f0719c3d203cc5c6088ae2c214daccf
Johannes Waldmann <joe>
2004-12-08 01:24:28 +0000

    files

---
+++ src/Exp/Test.hs
@@ -0,0 +1,42 @@
+module Exp.Test where
+
+--  $Id$
+
+import Exp.Property
+
+import Autolib.Exp
+import Autolib.Exp.Einfach
+import Autolib.Exp.Sanity
+
+import Autolib.Reporter
+import Autolib.Reporter.Set
+import Autolib.ToDoc
+import Autolib.Reader
+import Autolib.Size
+import Autolib.Symbol
+
+test :: ( Symbol c, Reader [c], ToDoc [c] )
+     => Property c 
+     -> RX c
+     -> Reporter ()
+
+test (Min_Size s) exp = do
+    assert ( size exp >= s ) 
+	   $ text "Größe des Ausdrucks ist wenigstens" <+> toDoc s <+> text "?"
+test (Max_Size s) exp = do
+    assert ( size exp <= s ) 
+	   $ text "Größe des Ausdrucks ist höchstens" <+> toDoc s <+> text "?"
+
+test (Alphabet a) exp = do
+    sanity_alpha a exp
+
+test (Simple) exp = do
+    ist_einfach exp
+
+test (Extended) exp = do
+    ist_erweitert exp
+
+test prop exp = do
+    reject $ fsep [ text "test für", toDoc prop
+		  , text "noch nicht implementiert"
+		  ]
