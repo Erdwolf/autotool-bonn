4556f07574edf541a17003848cadab68015cb0f4
Mirko Rahn <mrahn>
2005-03-21 14:51:53 +0000

    - standardconfig besser - bibliotheksroutinen aus Autolib.Util.Zufall verwendet

--- src/Graph/Bi/Quiz.hs
+++ src/Graph/Bi/Quiz.hs
@@ -3,6 +3,7 @@ module Graph.Bi.Quiz where
 --  $Id$
 
 import Autolib.Graph.Type
+import Autolib.Util.Zufall ( eins , genau )
 import Graph.Bi.Plain
 import Graph.Bi.Config
 
@@ -19,12 +20,12 @@ instance Generator Bi Config ( Graph Int , Set Int ) where
 
        let ns = [ 1 .. nodes conf ]
 
-       froms <- n_of (div (nodes conf) (teil conf)) ns
+       froms <- genau (div (nodes conf) (teil conf)) ns
 
        let tos = filter (not. flip elem froms) ns
 
-       ks <- mapM ( \ _ -> do from <- one_of froms
-		              to   <- one_of tos
+       ks <- mapM ( \ _ -> do from <- eins froms
+		              to   <- eins tos
 		              return $ kante from to
 		  ) [ 1 .. edges conf ]
 
@@ -36,14 +37,3 @@ instance Project Bi ( Graph Int, Set Int ) ( Graph Int ) where project _ = fst
 
 make :: Make
 make = quiz Bi rc
-
-----------------------------------------------------------------------------------------------------
-
-one_of :: [a] -> IO a
-one_of xs = randomRIO ( 0 , pred $ length xs ) >>= return . (!!) xs
-
-n_of :: Eq a => Int -> [a] -> IO [a]
-n_of 0 _  = return []
-n_of n xs = do x <- one_of xs
-	       ys <- n_of (pred n) $ filter ( /= x ) xs
-	       return $ x : ys
