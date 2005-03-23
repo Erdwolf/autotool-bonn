b035963eaea71f03d604f87e8fcb7e34fb6632a6
Mirko Rahn <mrahn>
2005-03-23 10:06:39 +0000

    hamiltonsche wege

---
+++ src/Graph/Ham.hs
@@ -0,0 +1,73 @@
+-- $Id$
+
+module Graph.Ham where
+
+-- | hamiltonsche wege: wege, die jeden knoten genau einmal berühren
+
+import Autolib.Graph.Type ( Graph , Kante , kante , von , nach , knoten 
+			  , mkGraph 
+			  )
+import Autolib.Graph.Util ( knotenliste )
+import Autolib.Set ( Set , mkSet , addToSet , elementOf )
+import Graph.Util ( lnachbarn )
+import Control.Monad ( guard )
+
+-------------------------------------------------------------------------------
+
+-- | ein weg ist als eine (nichtleere) kantenfolge definiert
+
+type Weg a = [Kante a]
+
+-------------------------------------------------------------------------------
+
+-- | jedem weg, der an einem knoten n beginnt, ist eindeutig eine folge
+-- | von knoten zugeordnet, die durchlaufen werden
+
+knotenfolge :: Eq a => a -> Weg a -> [a]
+knotenfolge n [k]    | von k == n = [n,nach k]
+		     | otherwise  = [n,von  k]
+knotenfolge n (k:ks) | von k == n = n : knotenfolge (nach k) ks
+		     | otherwise  = n : knotenfolge (von  k) ks
+knotenfolge _ _ = []
+
+-------------------------------------------------------------------------------
+
+-- | alle hamiltonschen wege als weg und als knotenfolge
+
+alle_hamiltons :: Ord a => Graph a -> [(Weg a,[a])]
+alle_hamiltons g = do n <- knotenliste g
+		      w <- build [] (mkSet [n]) g n
+		      return (w,knotenfolge n w)
+
+-------------------------------------------------------------------------------
+
+build :: Ord a => Weg a -> Set a -> Graph a -> a -> [Weg a]
+build ks ns g n
+    | ns == knoten g = [ks]
+    | otherwise      = concat $ do 
+		       m <- lnachbarn g n
+		       guard $ not $ elementOf m ns
+		       let k = kante n m
+		       guard $ not $ elem k ks
+		       return $ build (ks ++ [k]) (addToSet ns m) g m
+
+-------------------------------------------------------------------------------
+
+-- | beispielgraphen
+
+a, b, c :: Graph Int
+a = mkGraph (mkSet [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])
+	    (mkSet [ kante 1 9, kante 2 9, kante 3 9, kante 4 9
+		   , kante 5 9, kante 6 9, kante 7 9, kante 8 9
+		   ]
+	    )
+b = mkGraph (mkSet [ 1, 2, 3, 4, 5, 6, 7 ])
+            (mkSet [ kante 1 2, kante 1 3, kante 2 4, kante 2 5
+                   , kante 4 6, kante 4 7, kante 5 7
+                   ]
+	    )
+c = mkGraph (mkSet [ 1, 2, 3, 4, 5, 6 ])
+	    (mkSet [ kante 1 2, kante 1 4, kante 1 6, kante 2 3
+                   , kante 2 5, kante 3 4, kante 3 6, kante 4 5, kante 5 6
+                   ]
+	     )
