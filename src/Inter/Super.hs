aa6e0d8dc62a599738c4e9df389bc48f50ba10cf
Johannes Waldmann <joe>
2005-10-21 09:10:39 +0000

    link to scores

--- src/Inter/Super.hs
+++ src/Inter/Super.hs
@@ -81,10 +81,11 @@ main = Inter.CGI.execute ( Local.cgi_name ++ "#hotspot" ) $ do
    footer scores
 
 slink = do
-    e <- doesFileExist "link.scores"
-    scores <- if e 
-              then readFile "link.scores" >>= return . head . lines
-              else return "http://www.imn.htwk-leipzig.de/~autotool/scores"
+--    e <- doesFileExist "link.scores"
+    scores <- 
+        if False 
+        then readFile "link.scores" >>= return . head . lines
+        else return "http://autotool.imn.htwk-leipzig.de/high/scores.text"
     return scores
 
 
