07b6f576ef0fd8f5b3b9466a8f7216a5b961e9fb
Johannes Waldmann <joe>
2005-11-28 15:46:30 +0000

    TH

---
+++ src/Inter/Util.hs
@@ -0,0 +1,15 @@
+module Inter.Util where
+
+import qualified Inter.Param as P
+import qualified Control.Aufgabe as A
+import qualified Control.Student as S
+
+mkpar stud auf = P.empty 
+            { P.mmatrikel = Just $ S.mnr stud
+	    , P.aufgabe = A.name auf
+	    , P.typ = A.typ auf
+	    , P.anr = A.anr auf
+	    , P.vnr = A.vnr auf
+	    , P.highscore = A.highscore auf
+	    , P.ident = S.snr stud
+            }
