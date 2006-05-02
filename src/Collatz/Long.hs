d9a061593ce484a33155fc384c3ff70f174a0382
Johannes Waldmann <joe>
2006-05-02 20:16:05 +0000

    require full parameters

--- src/Collatz/Long.hs
+++ src/Collatz/Long.hs
@@ -19,24 +19,24 @@ import Autolib.Reporter
 
 data Collatz_Long = Collatz_Long deriving ( Eq, Ord, Show, Read, Typeable )
 
-instance C.Partial Collatz_Long () (Integer, Integer) where
+instance C.Partial Collatz_Long () (Integer, P.Parameter) where
 
     report Collatz_Long () = do
         inform $ vcat
 	       [ text "Gesucht ist eine lange Collatz-Folge."
-               , text "Geben Sie ein Paar (Startzahl, Länge) ein."
-               , text "Bewertet wird  Länge - log_2(Startzahl)."
+               , text "Geben Sie ein Paar (Startzahl, Parameter) ein."
+               , text "Bewertet wird  Parameter.length - log_2(Startzahl)."
 	       ]
 
-    initial Collatz_Long () = ( 7, 16 )
+    initial Collatz_Long () = ( 7, P.compute 7 )
 
-    total Collatz_Long () ( start, len) = do
-	assert ( len == P.length ( P.compute start ) )
+    total Collatz_Long () ( start, p ) = do
+	assert ( P.compute start == p )
 	       $ text "angegebene Parameter sind korrekt?"
 
-instance C.Measure Collatz_Long () (Integer, Integer) where
-    measure Collatz_Long () ( start, len) = 
-        max 0 $ len - log2 start 
+instance C.Measure Collatz_Long () (Integer, P.Parameter) where
+    measure Collatz_Long () ( start, p ) = 
+        max 0 $ P.length p - log2 start 
 
 make :: Make
 make = direct Collatz_Long ( )
