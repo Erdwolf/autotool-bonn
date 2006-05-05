6200ab17bf1613559ef0ae310f40ad4a412b7b3e
Johannes Waldmann <joe>
2006-05-05 09:14:40 +0000

    bound length

--- src/Collatz/Long.hs
+++ src/Collatz/Long.hs
@@ -19,27 +19,33 @@ import Autolib.Reporter
 
 data Collatz_Long = Collatz_Long deriving ( Eq, Ord, Show, Read, Typeable )
 
-instance C.Partial Collatz_Long () (Integer, P.Parameter) where
 
-    report Collatz_Long () = do
+
+instance C.Partial Collatz_Long Integer (Integer, P.Parameter) where
+
+    report Collatz_Long top = do
         inform $ vcat
 	       [ text "Gesucht ist eine lange Collatz-Folge."
                , text "Geben Sie ein Paar (Startzahl, Parameter) ein."
                , text "Bewertet wird  Parameter.length - log_2(Startzahl)."
+               , text "Die Startzahl soll höchstens" <+> toDoc top 
+                          <+> "Dezimalstellen haben." 
 	       ]
 
-    initial Collatz_Long () = ( 7, P.compute 5 )
+    initial Collatz_Long top = ( 7, P.compute 5 )
 
-    total Collatz_Long () ( start, p ) = do
+    total Collatz_Long top ( start, p ) = do
+        assert ( length ( show start ) <= top )
+               $ text "Startzahl nicht zu groß?"
 	assert ( P.compute start == p )
 	       $ text "angegebene Parameter sind korrekt?"
 
-instance C.Measure Collatz_Long () (Integer, P.Parameter) where
-    measure Collatz_Long () ( start, p ) = 
+instance C.Measure Collatz_Long Integer (Integer, P.Parameter) where
+    measure Collatz_Long top ( start, p ) = 
         max 0 $ P.length p - log2 start 
 
 make :: Make
-make = direct Collatz_Long ( )
+make = direct Collatz_Long 1000
 
 log2 :: Integer -> Integer
 log2 n = if n <= 1 then 0 else succ $ log2 ( n `div` 2 )
