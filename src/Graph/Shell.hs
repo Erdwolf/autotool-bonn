3c3e6a37f4455c20de33d70bfa2e1dd9e9f8d58e
Johannes Waldmann <joe>
2006-11-17 15:05:38 +0000

    formatting choice

--- src/Graph/Shell.hs
+++ src/Graph/Shell.hs
@@ -16,16 +16,22 @@ main :: IO ()
 main = Gateway.CGI.execute "Graph.cgi" $ Gateway.CGI.wrap $ do
     open table
     input <- defaulted_textarea "Graph expression" $ show Graph.Op.example
-    open row ; submit "submit" ; close -- row
+    -- open row ; submit "submit" ; close -- row
+    lout <- click_choice "Layouter" $ do
+            lout <- [  Dot , Neato , Twopi , Circo , Fdp ]
+            return ( show lout, lout )
     close -- table
-    ( res, com :: Html ) <- io $ run $ handler input
+    ( res, com :: Html ) <- io $ run $ handler input lout
     html com
 
-handler input = do
+handler input lout = do
     exp :: Exp Graph_Or_Natural <- parse_or_complain input
     Gra g <- return $ eval0 exp
     inform $ toDoc g
-    peng $ g { layout_program = Dot
-             , layout_hints = [ "-Nshape=ellipse" , "-Nsize=\"1\"" ]
+    peng $ g { layout_program = lout
+             , layout_hints = [ "-Nheight=0.1", "-Nwidth=0.1"
+                              , "-Nfixedsize=true"
+                              , "-Gsize=5,5"
+                              ]
              }
 
