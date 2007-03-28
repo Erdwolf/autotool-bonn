2e8de4e42fac99e250e83ebb9d04060be9c48f0d
Johannes Waldmann <joe>
2007-03-28 12:55:23 +0000

    typing

--- src/FP/Reconstruct.hs
+++ src/FP/Reconstruct.hs
@@ -18,6 +18,15 @@ import Autolib.ToDoc
 
 type Env = FiniteMap Identifier Type
 
+env0 :: Env
+env0 = listToFM 
+    [ ( read "map", read "forall a b . (a -> b) -> [a] -> [b]" )
+    , ( read "append", read "forall a . [a] -> [a] -> [a]" )
+    , ( read "reverse", read "forall a. [a] -> [a]" )
+    , ( read "nil", read "forall a. [a]" )
+    , ( read "c", read "forall a b c . (a -> b) -> (b -> c) -> (a -> c)" )
+    ]
+
 look :: Env -> Identifier -> Reporter Type
 look env at = do
     case lookupFM env at of
@@ -30,14 +39,18 @@ reconstruct ::  Env
 	    -> Expression Identifier
 	    -> Reporter Type
 reconstruct env x = do
+    t0 <- reconstructor env x
+    return $ wrap t0
+
+reconstructor env x = do
     inform $ text "find most general type for" <+> toDoc x
     t0 <- nested 4 $ reconstruct0 env x
-    let t = wrap t0
     inform $ vcat 
 	   [ text "most general type for" <+> toDoc x
-	   , text "is" <+> toDoc t
+	   , nest 4 $ text "is" <+> toDoc ( wrap t0 )
 	   ]
-    return t 
+    return t0
+    
 
 reconstruct0 :: Env
 	    -> Expression Identifier
@@ -46,10 +59,12 @@ reconstruct0 env ( Atomic at ) = do
     t <- look env at    
     return $ core t
 reconstruct0 env ( Apply fun arg ) = do
-    tfun <- reconstruct0 env fun
+    tfun <- reconstructor env fun
+
+    inform $ text "soll ein Funktionstyp sein"
     let [ src, tgt ] = unused 2 $ vars $ unArrow tfun
     let ar = Node ( mkunary "->" ) [ Var src, Var tgt ]
-    inform $ text "soll ein Funktionstyp sein"
+
     sigma <- case mgu ar $ unArrow tfun of
         Just sigma -> do
 	   inform $ text "OK mit Substitution" <+> toDoc sigma
@@ -58,15 +73,16 @@ reconstruct0 env ( Apply fun arg ) = do
     let src1 = apply sigma $ Var src
 	tgt1 = apply sigma $ Var tgt
 
-    targ <- reconstruct0 env arg
-    inform $ text "soll passen zum Argumenttyp" <+> toDoc src1
+    -- TODO: typvariablen in arg wegbenennen (konfliktfrei mit fun)
+    targ <- reconstructor env arg
+    inform $ text "soll passen zum Argumenttyp" <+> toDoc src1 
     rho <- case mgu ( unArrow targ ) src1 of
         Just rho -> do
 	    inform $ text "OK mit Substitution" <+> toDoc rho
 	    return rho
 	Nothing -> reject $ text "Falsch"
 
-    return $ Arrow $ apply rho tgt1
+    return $ Arrow $ apply_partial rho tgt1
 
 
 
