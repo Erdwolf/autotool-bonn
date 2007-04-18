29b195839109c4657726e51c5b437d4ffc7c9f06
Johannes Waldmann <joe>
2007-04-18 22:49:30 +0000

    infinite und pre-

--- src/NFA/Infinite.hs
+++ src/NFA/Infinite.hs
@@ -19,9 +19,26 @@ infinite_states a =
              (p,q) <- R.pairs t
              guard $ p == q
              return p
-    in  R.simages ( R.trans $ R.inverse r ) loops
+    in  R.simages ( R.reflex_trans $ R.inverse r ) loops
 
 is_infinite ::  NFAC c s 
                 => NFA c s -> Bool
 is_infinite a = not $ isEmptySet 
     $ intersect ( infinite_states a ) ( starts a )
+
+-- | set of states q such that L(start -> q) is infinite
+pre_infinite_states :: NFAC c s 
+                => NFA c s -> Set s
+pre_infinite_states a =
+    let b = Autolib.NFA.trim a
+        r = R.make $ do 
+                (p,c,q) <- unCollect $ trans a
+                return (p,q)
+        t = R.trans r
+        loops = mkSet $ do 
+             (p,q) <- R.pairs t
+             guard $ p == q
+             return p
+    in  R.simages ( R.reflex_trans $  r ) loops
+
+
