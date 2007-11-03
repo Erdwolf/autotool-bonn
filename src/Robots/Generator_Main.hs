f3d1cdebc9a101d2f360552a87e592b77de07f69
Johannes Waldmann <joe>
2007-11-03 12:49:45 +0000

    version

--- src/Robots/Generator_Main.hs
+++ src/Robots/Generator_Main.hs
@@ -13,6 +13,7 @@ import Robots.Move
 import Autolib.ToDoc
 import Autolib.Schichten
 import Autolib.Util.Zufall ( eins, repeat_until )
+import Autolib.Util.Sort 
 
 import Control.Monad ( when, guard ) 
 import System.Environment
@@ -25,17 +26,46 @@ main :: IO ()
 main = do
     [ n, w ] <- getArgs
     top <- newIORef 0
-    sequence_ $ repeat $ action1 top ( read n ) ( read w )
+    sequence_ $ repeat $ action2 top ( read n ) ( read w )
+
+
+action2 top n w = do
+    start0 <- some_without_target n $ border w
+    let start = attach_target_to_first (0,0) start0
+    print $ vcat [ nice start , text $ replicate 50 '*' ]
+    let ks = find start
+    mapM_ ( \ (b,c,zs) -> when ( is_shifted start c ) $ do
+        print $ vcat [ besides [ nice start, nice c ]
+				       , toDoc $ reverse zs 
+				       ]
+	error "huh"
+      ) ks
+
+
+find k = takeUntil ( \ (b,c,_) -> is_shifted k c )
+       $ take 1000
+       $ tail
+       $ search znachfolger_all_onboard badness k
+
+is_shifted k0 k1 = 
+    let smp = sort . map position . robots
+        diffs = zipWith (\ (a,b) (c,d) -> (a-c,b-d)) ( smp k0 ) ( smp k1 )
+        tag (a,b) = if abs a == abs b then abs a else 0
+        all_equal xs = 1 == length ( sort xs )
+    in  k0 /= k1 && all_equal ( map tag diffs )
+
+--------------------------------------------------------------------------
 
 action1 top n w = do
-    let border = do
+    mid0 <- some_without_target n $ border w
+    let mid = attach_target_to_first (0,0) mid0
+    handle top [ mid ]
+
+border w = do
 	    p @ (x,y) <- range ((-w,-w),(w,w))
 --	    guard $ abs x > w - 2 || abs y > w - 2
 	    guard $ abs x > w - 3 && abs y > w - 3
 	    return p
-    mid0 <- some_without_target n border
-    let mid = attach_target_to_first (0,0) mid0
-    handle top [ mid ]
 
 attach_target_to_first p k = make $ 
     let r : rs = robots k
@@ -86,3 +116,4 @@ handle top is = do
 	    putStrLn $ "length: " ++ show ( length zs )
 	    hFlush stdout
 	    writeIORef top $ length zs
+
