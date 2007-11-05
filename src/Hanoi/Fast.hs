15a49462a0d4cd2d0d50a25872e3faabcaac3b2f
Johannes Waldmann <joe>
2007-11-05 21:44:13 +0000

    version for tabulation

--- src/Hanoi/Fast.hs
+++ src/Hanoi/Fast.hs
@@ -3,45 +3,53 @@
 module Hanoi.Fast where
 
 import Hanoi.QSearch
-import Hanoi.Type
 
 import Autolib.ToDoc
 
 import Data.List
 import Control.Monad ( guard )
+import System.IO
 
+type Turm = Int -- ^ argh
 type State = [ Turm ]
 
-handle k = do
-    let targets = -- neighbours
-                  cyclic
-    let start = replicate k minBound
-    let final = replicate k maxBound
-    let badness zs this = 
-            let ( ok, unok ) = span ( == maxBound ) $ reverse this
-            in  if this == final
+type Zug = (Turm, Turm)
+
+tabulate targets p s = besides $ do
+    k <- [ 0 .. s ]
+    return $ vcat $ do
+        q <- [ 3 .. p ]
+        return $ case solve ( targets q ) q k of
+            Nothing -> text "*"
+            Just n  -> toDoc n
+
+solve targets pegs pieces = 
+    let start_peg = 0
+        start = replicate pieces start_peg
+        goal_peg  = pegs - 1
+        goal = replicate pieces goal_peg
+        badness zs this = 
+                if this == goal
                 then 0
                 else 1 + fromIntegral ( length zs )
-    let cs = decreasing
-                $ search ( moves targets )  badness $ start 
-    let obfuscate zs = zs ++ concat ( replicate 20 [ (D,C),(C,D) ] )
-    sequence_ $ do
-        ( v, s, zs ) <- cs
-        return $ print $ vcat
-            [ empty
-            , toDoc -- $ obfuscate
-                    $ zs 
-            , toDoc s
-            , toDoc ( length zs )
-            ]
+        
+    in  case   filter ( \ (b,c,zs) -> c == goal )
+             $ take 100000
+             $ search ( moves targets )  badness $ start of
+            ( b, c, zs ) : _ -> Just $ length zs 
+            _ -> Nothing
 
-cyclic t = 
-    [ if t < maxBound then succ t else minBound ]
+central top t = 
+    if t == 0 then [ 1 .. top - 1 ] else [ 0 ]
 
-neighbours t = 
-        [ pred t | t > minBound ]
-     ++ [ succ t | t < maxBound ]
+cyclic top t = [ succ t `mod` top ]
+
+neighbours top t = 
+        [ pred t | t > 0 ]
+     ++ [ succ t | t < top - 1 ]
     
+free top t = filter ( /= t ) [ 0 .. top - 1 ]
+
 moves :: ( Turm -> [Turm] ) -> State -> [ ( Zug, State ) ]
 moves targets this = do
     ( pre, t : post ) <- splits this
