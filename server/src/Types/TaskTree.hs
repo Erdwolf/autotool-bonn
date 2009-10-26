28a9659e7153df3363e5ca6e5a162228357018c5
Bertram Felgenhauer <int-e@gmx.de>
2009-10-26 08:30:22 +0100

    add field names to TaskTree type

--- server/src/Types/TaskTree.hs
+++ server/src/Types/TaskTree.hs
@@ -8,7 +8,8 @@ import Types.Basic
 
 import Data.Autolib.Transport
 
-data TaskTree = Task Task
-              | Category Name [TaskTree]
+data TaskTree = Task     { task_name :: Task }
+              | Category { category_name :: Name,
+                           sub_trees :: [TaskTree] }
 
 $(derives [makeToTransport] [''TaskTree])
