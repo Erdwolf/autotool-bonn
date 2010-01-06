fdcb5285b30d739742cedb765d50dff803496ba0
Bertram Felgenhauer <int-e@gmx.de>
2010-01-06 20:55:35 +0100

    Avoid duplicate application of rules in CL.Reduce.step

--- src/CL/Reduce.hs
+++ src/CL/Reduce.hs
@@ -11,15 +11,15 @@ import Control.Monad ( guard )
 
 step :: Map Identifier Combinator
      -> Term -> [ Term ]
-step coms t = here coms t ++ do
-    ( con, s ) <- drop 1 $ positions t
-    s' <- step coms s
+step coms t = do
+    ( con, s ) <- positions t
+    s' <- here coms s
     return $ con s'
    
 here coms t = do
     Sym x : xs <- return $ spine t
     c <- maybeToList $ M.lookup x coms
-    guard $ arity c <= length xs
+    guard $ arity c == length xs
     let ( pre, post ) = splitAt ( arity c ) xs
         sub = M.fromList $ zip ( arguments c ) pre
     let s' = apply sub $ result c
