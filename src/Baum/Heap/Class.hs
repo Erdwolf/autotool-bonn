8caf6668c999f100942edb76f3d32b5b94ec2b63
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2010-12-07 15:05:15 +0100

    heap good

---
+++ src/Baum/Heap/Class.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+module Baum.Heap.Class where
+
+import Baum.Heap.Op
+import Autolib.ToDoc
+import Autolib.Reader
+import Autolib.Reporter
+import Autolib.Hash
+
+import Tree
+import Data.Typeable
+
+
+class Heap baum where
+
+    empty :: baum a
+    isEmpty :: baum a -> Bool 
+
+    insert :: Ord a => baum a -> a -> baum a
+    -- | precondition: tree is not empty
+    deleteMin :: Ord a => baum a -> baum a
+    -- | precondition: tree contains this position,
+    -- new element is smaller or equal to element at position in tree
+    decreaseTo :: Ord a => baum a -> Position -> a -> baum a
+
+    equal :: Eq a => baum a -> baum a -> Bool
+    contents :: Ord a => baum a -> [a]
+
+class ( Show t, Typeable t, Read t
+      , Heap baum, OpC a
+      , ToTree ( baum a )
+      , ToDoc (baum a), Show ( baum a )
+      , Reader ( baum a ) , Read ( baum a )
+      , ToDot ( baum a), Eq (baum a), Hash (baum a)
+      , Typeable (baum a)
+      , Typeable a
+      ) 
+    => Tag t baum a | t -> baum, t -> a where
+   tag :: t
+
+
+
