5e6f54cc996134f251eb1b452d5df84ac78c1250
Florian Pilz <fpilz87@googlemail.com>
2010-12-21 10:45:00 +0100

    finished implementation of interface Heap for ListTree

--- src/Baum/List/Ops.hs
+++ src/Baum/List/Ops.hs
@@ -4,6 +4,13 @@ import Baum.List.Type
 import Baum.Heap.Class
 import Autolib.Size
 
+toListHelper t a = case t of
+  Nil -> []
+  (Cons x xs) -> (a, x) : toListHelper xs (0 : a)
+  
+deleteAtPosition (Cons x xs) p = case p of
+  [] -> xs
+  c : q -> Cons x $ deleteAtPosition xs q
 
 instance Heap ListTree where
 
@@ -14,18 +21,32 @@ instance Heap ListTree where
     isEmpty t = case t of Nil -> True ; _ -> False
 
     -- insert :: Ord a => baum a -> a -> baum a
-    insert t x = Cons x t
+    insert t a = case t of
+      Nil -> Cons a t
+      (Cons x xs) -> if (a < x)
+                     then Cons a (Cons x xs)
+                     else Cons x $ insert xs a
 
     -- deleteMin :: Ord a => baum a -> baum a
     deleteMin ( Cons x xs) = xs
 
     -- decreaseTo :: Ord a => baum a -> Position -> a -> baum a
-    decreaseTo (Cons x xs) p y = case p of
-        [] -> Cons y xs
-        c : q -> Cons x ( decreaseTo xs q y )
+    decreaseTo (Cons x xs) p y = insert (deleteAtPosition (Cons x xs) p) y
 
     -- equal :: Eq a => baum a -> baum a -> Bool
-    equal x y = True
+    equal a b = case a of
+      Nil -> case b of
+        Nil -> True
+        _  -> False
+      (Cons x xs) -> case b of
+        Nil -> False
+        (Cons y ys) -> if (x == y)
+                       then equal xs ys
+                       else False
+
+    toList t = case t of
+      Nil -> []
+      (Cons x xs) -> toListHelper t []
     
     -- contents :: Ord a => baum a -> [a]
     contents t = case t of
