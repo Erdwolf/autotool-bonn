5abb09138932398ec4dd945bf7b303c4fafbff7e
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-05-14 17:38:20 +0200

    Exchange of interpreter code and cleanup.

--- src/Haskell/Blueprint/Central.hs
+++ src/Haskell/Blueprint/Central.hs
@@ -6,53 +6,30 @@ import Debug ( debug )
 
 import Haskell.Blueprint.Data
 import Haskell.Blueprint.Match
+import Haskell.Blueprint.TestHelperContents (testHelperContents)
 
-import Language.Haskell.Exts as Hs
-import qualified Language.Haskell.Exts.SrcLoc as S
+import Language.Haskell.Exts
+import Language.Haskell.Exts.SrcLoc (srcLine, srcColumn)
 
-import qualified Mueval.ArgsParse as M
-import qualified Mueval.Interpreter
-import qualified Language.Haskell.Interpreter as Hint
-import qualified Mueval.Context
-
-import Challenger.Partial
-import Autolib.ToDoc
-import Autolib.Reader hiding (parse)
-import Autolib.Reporter.IO.Type
-import qualified Autolib.Reporter as R
-import Inter.Types
-import Inter.Quiz
-import Data.Typeable (Typeable)
-
-import qualified Control.Exception
-import Control.Monad.IO.Class
-import Control.Monad.Trans (MonadIO)
-import Test.SmallCheck
-import System.IO.Temp
-import System.IO.UTF8
-import System.Random ( randomRIO )
+import Language.Haskell.Interpreter
+import System.Directory (setCurrentDirectory, getTemporaryDirectory)
+import System.FilePath (takeBaseName, takeExtension)
 import qualified System.FilePath as Path
-import System.FilePath.Posix (takeFileName)
-import qualified System.IO.Strict
-import qualified System.IO
-import qualified System.Directory
-import System.Directory (copyFile, makeRelativeToCurrentDirectory, removeFile, setCurrentDirectory)
-import qualified System.Posix.Directory as SPD
+import Data.List (union, isPrefixOf, groupBy)
+import Control.Monad (when, forM)
+import System.IO.Temp (withTempDirectory)
 
-import Data.List
-import Control.Monad
+import Challenger.Partial (Verify(..), Partial(..))
+import Autolib.ToDoc (derives, makeToDoc, text, vcat, toDoc, nest, ToDoc(..))
+import Autolib.Reader (makeReader, Reader(..), {- only needed inside derived code: -} readerParenPrec, my_reserved, pzero, (<|>))
+import Autolib.Reporter (reject, inform)
+import qualified Autolib.Reporter.IO.Type (reject, inform)
+import Data.Typeable (Typeable)
+import Inter.Types (OrderScore(..), ScoringOrder(..), direct)
 
+rejectIO = Autolib.Reporter.IO.Type.reject
+informIO = Autolib.Reporter.IO.Type.inform
 
---import Control.Monad (guard,mplus,unless,when)
-import qualified Control.Exception.Extensible as E (evaluate,catch,SomeException(..))
-
-import Language.Haskell.Interpreter (eval, set, reset, setImportsQ, loadModules, liftIO,
-                                     installedModulesInScope, languageExtensions,
-                                     typeOf, setTopLevelModules, runInterpreter, glasgowExtensions,
-                                     OptionVal(..), Interpreter,
-                                     InterpreterError(..),GhcError(..))
-
-import qualified Mueval.Context (qualifiedModules)
 
 
 data Haskell_Blueprint = Haskell_Blueprint deriving Typeable
@@ -71,7 +48,7 @@ instance Partial Haskell_Blueprint Code Code where
     describe p i = vcat
         [ text "Vervollständigen Sie das Haskell-Programm."
         , text "Ersetzen Sie jedes 'undefined',"
-        , text "so daß die Tests erfolgreich sind."
+        , text "so dass die Tests erfolgreich sind."
         , nest 4 $ toDoc i
         ]
     initial p (Code i) = Code $ blueprintSegment i
@@ -79,65 +56,48 @@ instance Partial Haskell_Blueprint Code Code where
     partial p ( Code i ) ( Code b ) = do
         mi <- parseM (blueprintSegment i)
         mb <- parseM b
-        --R.inform $ text $ show mi
-        --R.inform $ text $ show mb
-        R.inform $ text "paßt Ihr Quelltext zum Muster?"
+        --inform $ text $ show mi
+        --inform $ text $ show mb
+        inform $ text "paßt Ihr Quelltext zum Muster?"
         case Haskell.Blueprint.Match.test mi mb of
             Fail loc ->  reject_parse b loc "Nein"
-            Haskell.Blueprint.Match.Ok _ -> R.inform $ text "Ja."
+            Haskell.Blueprint.Match.Ok _ -> inform $ text "Ja."
 
     totalIO p (Code i) (Code b) = do
-        r <- liftIO $ do
-            tmp <- System.Directory.getTemporaryDirectory
+        result <- liftIO $ do
+            tmp <- getTemporaryDirectory
             withTempDirectory tmp "Blue" $ \dirname -> do
                let ParseOk namedModules = fmap (map (\(k,v) -> (k,if k == "Blueprint" then b else v))) $ withNames (splitModules i)
                files <- namedModules `forM` \(name,contents) -> do
                   let fname = dirname Path.</> (name ++ ".hs")
-                  System.IO.UTF8.writeFile fname contents
+                  writeFile fname contents
                   return fname
-               flip Control.Exception.catch (\( e :: Control.Exception.SomeException ) -> do
-                        debug $ "interpreter got exception " ++ show e
-                        return $ Left $ Hint.UnknownError ( show e )) $ do
-                  Hint.runInterpreter $ do
-                        let exts = []
-                        let modules = ["Prelude"]
-                        let expr = "test"
 
-                        set [languageExtensions := (glasgowExtensions ++ map read exts)]
+               writeFile (dirname Path.</> "Information.hs") $ "module Information where filePath = \"Blueprint.hs\""
+               let existingModules = map takeBaseName $ filter ((".hs"==).takeExtension) $ filter (`notElem` [".",".."]) files
+                   modules = union ["Test"] existingModules
+               when (notElem "Test" existingModules) $ do
+                  writeFile (dirname Path.</> "Test.hs") $ "module Test (test) where\nimport qualified Blueprint (test)\ntest = Blueprint.test"
+               writeFile (dirname Path.</> "TestHelper.hs") testHelperContents
+               runInterpreter $ do
+                     liftIO $ setCurrentDirectory dirname -- will at least mess up relative links
+                     reset -- Make sure nothing is available
 
-                        reset -- Make sure nothing is available
-                        set [installedModulesInScope := False]
-                        liftIO $ setCurrentDirectory dirname -- will at least mess up relative links
+                     set [languageExtensions := map read []]
+                     set [installedModulesInScope := False]
 
-                        loadModules [ name ++ ".hs" | (name,_) <- namedModules ]
-                        setTopLevelModules (map fst namedModules)
+                     loadModules modules
+                     setTopLevelModules modules
+                     setImportsQ [("Prelude",Nothing)]
 
-                        let unqualModules = zip modules (repeat Nothing)
-                        setImportsQ (unqualModules ++ Mueval.Context.qualifiedModules)
+                     interpret "run Test.test" (as :: Maybe (Bool, String))
 
-                        -- we don't check if the expression typechecks
-                        -- this way we get an "InterpreterError" we can display
-                        etype <- typeOf expr
-                        result <- eval "test"
-
-                        return (expr, etype, result)
-        case r of
-            Left (Hint.WontCompile err) -> reject $ vcat $ map (text . Hint.errMsg) err
-            Left err -> reject $ text $ show err
-            Right ( e, et, val ) -> do
-                inform $ vcat
-                       [ text "expression" </> text e
-                       , text "type" </> text et
-                       ]
-                assert ( et == "Bool" ) $ text "richtiger Typ?"
-
-                v <- liftIO  ( ( do Control.Exception.evaluate val ; return ( Right val ) )
-                  `Control.Exception.catch` \ ( e :: Control.Exception.SomeException ) -> do
-                        return $ Left ( show e ) )
-
-                case v of
-                     Right val -> assert ( val == "True" ) $ text "richtiger Wert?"
-                     Left ex -> reject $ text "Exception" </> text ex
+        case result of
+           Right Nothing -> informIO $ text "ok."                     -- success
+           Right (Just (True,  msg)) -> rejectIO $ text msg           -- test case failure
+           Right (Just (False, msg)) -> rejectIO $ text msg           -- test case error
+           Left (WontCompile (GhcError msg:_)) -> rejectIO $ text msg -- compilation error(s), only showing the first one
+           Left err -> rejectIO $ text $ show err                     -- unexpected error (our fault)
 
 
 make_fixed = direct Haskell_Blueprint code_example
@@ -154,10 +114,10 @@ parseM m =
         ParseFailed loc msg -> reject_parse m loc msg
 
 reject_parse m loc msg =
-    let ( lpre, lpost ) = splitAt ( S.srcLine loc  ) $ lines m
+    let ( lpre, lpost ) = splitAt ( srcLine loc  ) $ lines m
         lpre' = reverse $ take 3 $ reverse lpre
-        tag = replicate ( S.srcColumn loc ) '.' ++ "^"
-    in  R.reject $ vcat ( map text lpre' ++ [ text tag, text msg ] )
+        tag = replicate ( srcColumn loc ) '.' ++ "^"
+    in  reject $ vcat ( map text lpre' ++ [ text tag, text msg ] )
 
 
 
@@ -175,4 +135,3 @@ withNames mods =
    fmap (flip zip mods) $ sequence $ map (fmap moduleName . parseFileContents) mods
 
 moduleName (Module _ (ModuleName name) _ _ _ _ _) = name
-
