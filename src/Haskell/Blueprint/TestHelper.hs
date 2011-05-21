5bd55380f6042ec4656ab482a1fe6ef65a55b6c3
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-05-21 14:16:14 +0200

    Better test helpers for Haskell.Blueprint

--- src/Haskell/Blueprint/TestHelper.hs
+++ src/Haskell/Blueprint/TestHelper.hs
@@ -1,14 +1,27 @@
-module TestHelper (qc) where
+{-# LANGUAGE ScopedTypeVariables #-}
+module TestHelper (qc, mustFail) where
 import Prelude
 import Data.List
 
 import Test.QuickCheck as QC
 import Test.HUnit as HU
 
+import Test.QuickCheck.Monadic (monadicIO, run)
+import Control.Exception (try, evaluate, SomeException)
+
 
 qc :: QC.Testable prop => prop -> HU.Assertion
 qc test = do
-   result <- quickCheckWithResult (stdArgs {chatty=False}) test
+   result <- quickCheckWithResult (stdArgs {chatty=False, maxSuccess=1000}) test
    case result of
       Success _ _ _ -> return ()
       _  -> assertFailure $ output result
+
+mustFail :: a -> String -> Property
+mustFail x msg =
+   monadicIO $ run $ do
+      resultOrError <- try (evaluate x)
+      case resultOrError of
+         Left (_::SomeException) -> return () -- expected failure occured.
+         Right _ -> error msg
+
