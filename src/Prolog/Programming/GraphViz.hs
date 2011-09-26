7c316035a3def10367d4ca48fbdf3f09b273e370
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-09-26 15:52:46 +0200

    Prolog.Programming: Thumbnail linking to full graph.

--- src/Prolog/Programming/GraphViz.hs
+++ src/Prolog/Programming/GraphViz.hs
@@ -4,27 +4,56 @@ module Prolog.Programming.GraphViz (resolveWithTree, asInlinePng) where
 import qualified Data.ByteString as BS
 import System.IO.Unsafe (unsafePerformIO)
 import qualified Codec.Binary.Base64 as Base64
-import Text.XHtml (showHtml, Html, image, (!), src, alt, width, height)
-import Data.GraphViz (graphvizWithHandle, GraphvizCommand(Dot), setDirectedness, graphToDot, nonClusteredParams, fmtNode, fmtEdge, toLabel, GraphvizOutput(Png))
+import Text.XHtml (showHtml, Html, image, (!), src, alt, anchor, href)
+import Data.GraphViz  --(runGraphviz, GraphvizCommand(Dot), setDirectedness, graphToDot, nonClusteredParams, fmtNode, fmtEdge, toLabel, GraphvizOutput(Png))
+import Data.GraphViz.Attributes.HTML (HtmlTextItem)
+import Data.GraphViz.Attributes.Complete
+import Data.GraphViz.Types (GlobalAttributes(GraphAttrs))
 import Language.Prolog (resolve_)
 import Language.Prolog.GraphViz (runGraphGenT)
+import System.Directory (getTemporaryDirectory)
+import System.FilePath ((</>),(<.>))
+import System.IO (openTempFile, hClose)
+import Control.Concurrent (threadDelay)
+import Data.Hashable (Hashable(hash))
 
+import Prolog.Programming.Hex (hex)
+
+import Data.Graph.Inductive.Graph (labNodes, labEdges)
+import Data.Graph.Inductive.Tree (Gr)
+
+instance (Hashable a, Hashable b) => Hashable (Gr a b) where
+   hash g = hash (labNodes g, labEdges g)
+
+instance Hashable HtmlTextItem where
+   hash = hash . show
 
 resolveWithTree p q = runGraphGenT $ resolve_ p q
 
-asInlinePng = showHtml . inlinePng . toPng
+asInlinePng = showHtml . toPng
 
+
+picsDir = ".."</>"pics"
 
 toPng graph = unsafePerformIO $ do
-   graphvizWithHandle Dot
-                      (setDirectedness graphToDot params graph)
-                      Png
-                      BS.hGetContents
+   --tmp <- getTemporaryDirectory
+   --(tmpfile, h) <- openTempFile tmp "graphviz"
+   --hClose h
+   let fname = (hex $ fromIntegral $ hash graph) ++ ".png"
+   runGraphviz (setDirectedness graphToDot params graph)
+               Png
+               (picsDir </> fname)
+   runGraphviz (setDirectedness graphToDot (params { globalAttributes = [GraphAttrs [Size $ createPoint 1 1]] }) graph)
+               Png
+               (picsDir </> "thumb_" ++ fname)
+   return $ anchor ! [ href ("../pics/" ++ fname) ]
+          $ image ! [ src ("../pics/thumb_" ++ fname), alt "Herleitungsbaum" ]
  where
    params = nonClusteredParams { fmtNode = \(_,l) -> [toLabel l]
                                , fmtEdge = \(_, _, l) -> [toLabel l]
                                }
 
+{-
 inlinePng :: BS.ByteString -> Html
 inlinePng pngImage =
      let
@@ -41,4 +70,4 @@ inlinePng pngImage =
                , width  (show w)
                , height (show h)
                ]
-
+-}
---
+++ src/Prolog/Programming/Hex.hs
@@ -0,0 +1,15 @@
+module Prolog.Programming.Hex (hex) where
+
+import Data.List (unfoldr)
+import Data.Word
+
+hex :: Word -> String
+hex = map ((['0'..'9']++['A'..'F'])!!) . map wordToInt . base 16
+
+wordToInt :: Word -> Int
+wordToInt = fromIntegral
+
+base b n = unfoldr uf n
+   where uf 0 = Nothing
+         uf x = Just (mod x b, div x b)
+
