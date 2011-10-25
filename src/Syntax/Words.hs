a6ccce4d469d74f56aa12b6e7ec90230c350ba63
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-10-25 16:36:14 +0200

    Syntax: Argh!

--- src/Syntax/Words.hs
+++ src/Syntax/Words.hs
@@ -10,22 +10,22 @@ import Data.List (nub)
 
 -- 'maxSteps" is the maximal number of production steps, not the number
 -- of generated words, although these are sometimes the same.
-generateWords maxSteps _ =
+generateWords maxSteps lang =
     iterate oneStep [[startSymbol]] !! maxSteps
  where
     terminal (T _) = True
     terminal _     = False
 
-oneStep ws = nub $ ws ++ (ws >>= produceNew)
+    oneStep ws = nub $ ws ++ (ws >>= produceNew)
 
-produceNew = liftM concat . mapM replace
+    produceNew = liftM concat . mapM replace
 
-replace (N t) = lookupAll t productions
-replace x = [[x]]
+    replace (N t) = lookupAll t productions
+    replace x = [[x]]
 
-startSymbol = N $ fst $ head productions
+    startSymbol = N $ fst $ head productions
 
-productions = map (\(Rule x y) -> (x,y)) $ toGrammar lang9
+    productions = map (\(Rule x y) -> (x,y)) $ toGrammar lang
 
-toGrammar = map toRule . removeForks . removeLoops
+    toGrammar = map toRule . removeForks . removeLoops
 
