06fadbd185c2dfcfc2da4f9eed3b4d201053c825
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-11-14 13:41:07 +0100

    ...

--- src/Program/GeneralBonn/Central.hs
+++ src/Program/GeneralBonn/Central.hs
@@ -49,7 +49,8 @@ instance ( ToDoc val, Reader val, Eq val
 
 
 
-make_fixed :: Class p st val => p -> Make
+make_fixed :: Class p st val, Reader ( Environment val ), ToDoc ( Environment val ) =>
+              p -> Make
 make_fixed p = direct 
        p
        ( example p )
