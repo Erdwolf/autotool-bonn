635e140101109db4cf34b67353c4d4a1a34542e2
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-11-28 12:41:17 +0100

    ...

--- src/HeapSort/Central.hs
+++ src/HeapSort/Central.hs
@@ -10,7 +10,7 @@ import Tree.Class (ToTree(..))
 import Debug ( debug )
 
 import Challenger.Partial (Verify(..), Partial(..))
-import Autolib.ToDoc (derives, makeToDoc, text, vcat, (<>), hsep, toDoc, nest, ToDoc(..))
+import Autolib.ToDoc (derives, makeToDoc, text, vcat, (<>), hsep, toDoc, nest, ToDoc(..), docParen, fsep, (</>))
 import Autolib.Reader (makeReader, Reader(..), {- only needed inside derived code: -} readerParenPrec, my_reserved, pzero, (<|>))
 import Autolib.Reporter (Reporter, reject, inform)
 import qualified Autolib.Reporter.IO.Type (reject, inform)
