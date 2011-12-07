296b1ce46d71c0e00b91d15e08b87e9fd818998e
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-07 13:29:33 +0100

    Baum.AVL: Don't show leaves

--- src/Baum/AVL/Show.hs
+++ src/Baum/AVL/Show.hs
@@ -1,7 +1,7 @@
 module Baum.AVL.Show where
 
-import Baum.AVL.Type as T
-import Data.Tree
+import Baum.AVL.Type (AVLTree (Leaf, Branch))
+import Data.Tree (Tree, unfoldTree)
 
 {-
 toTree :: Show a => AVLTree a -> Tree String
@@ -11,12 +11,12 @@ toTree = foldt
 -}
 
 toTree :: Show a => AVLTree a -> Tree String
-toTree = Data.Tree.unfoldTree uf
+toTree = unfoldTree uf
     where
-      uf (T.Branch T.Leaf x T.Leaf) = (show x,[])
-      uf (T.Branch l      x T.Leaf) = (show x,[l])
-      uf (T.Branch T.Leaf x r     ) = (show x,[r])
-      uf (T.Branch l      x r     ) = (show x,[l,r])
+      uf (Branch Leaf x Leaf) = (show x,[])
+      uf (Branch l    x Leaf) = (show x,[l])
+      uf (Branch Leaf x r   ) = (show x,[r])
+      uf (Branch l    x r   ) = (show x,[l,r])
 
 
 
