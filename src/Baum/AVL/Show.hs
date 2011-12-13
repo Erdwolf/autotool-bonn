d58c437f4d9312698023506a9f3e43dbe3d088e4
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-13 12:13:38 +0100

    Baum.AVL: trying things out.

--- src/Baum/AVL/Show.hs
+++ src/Baum/AVL/Show.hs
@@ -7,9 +7,10 @@ import Data.Tree (Tree, unfoldTree)
 toTree :: Show a => AVLTree a -> Tree String
 toTree = unfoldTree uf
     where
-      uf t | isLeaf l && isLeaf r = (show k,[])
-           |             isLeaf r = (show k,[l])
-           | isLeaf l             = (show k,[r])
+      uf t |       isLeaf t       = (" ",[])
+           | isLeaf l && isLeaf r = (show k,[])
+           |             isLeaf r = (show k,[l,r])
+           | isLeaf l             = (show k,[l,r])
            |       otherwise      = (show k,[l,r])
         where k = key t
               l = left t
