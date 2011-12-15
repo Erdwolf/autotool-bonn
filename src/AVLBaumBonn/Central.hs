32fdd7061d0e8cc7323af1dc947cf1af6beeca48
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-15 14:10:13 +0100

    AVLBaumBonn: specific changes made possible by extraction

--- src/AVLBaumBonn/Central.hs
+++ src/AVLBaumBonn/Central.hs
@@ -81,7 +81,7 @@ instance ToDot (Baum.AVL.Type.AVLTree Int) where
                        { Autolib.Dot.Edge.from     = show i
                        , Autolib.Dot.Edge.to       = show j
                        , Autolib.Dot.Edge.directed = True
-                       , Autolib.Dot.Edge.color    = maybe x (Just "white") (\_->Nothing)
+                       , Autolib.Dot.Edge.color    = maybe (Just "white") (\_->Nothing) x
                        }
 
         subtrees t = t : concatMap subtrees (subForest t)
