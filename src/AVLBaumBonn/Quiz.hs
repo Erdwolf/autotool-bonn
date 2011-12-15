55f903a4db4c8d16ce73cafba43515b6a62cdd38
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-15 13:02:45 +0100

    AVLBaumBonn: specific changes made possible by extraction

--- src/AVLBaumBonn/Quiz.hs
+++ src/AVLBaumBonn/Quiz.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, DeriveDataTypeable, TemplateHaskell #-}
 module AVLBaumBonn.Quiz where
 
 import Inter.Quiz
@@ -9,13 +9,28 @@ import qualified Baum.Such.Generate
 
 import AVLBaumBonn.Central
 
-type QuizConfig = Baum.Such.Config.Config Int
+data QuizConfig = QuizConfig
+	    { start_size :: Int
+        , min_key :: Int
+	    , max_key :: Int
+	    , fixed_insert_ops :: Int
+	    , guess_insert_ops :: Int
+	    }
+     deriving Typeable
+
+$(derives [makeReader, makeToDoc] [''QuizConfig])
 
 instance Generator AVLBaum QuizConfig Config where
-    generator _ conf _key = Baum.Such.Generate.generate conf
+    generator _ (QuizConfig ss l h f g _key =
+        Baum.Such.Generate.generate $ Baum.Such.Config.Config ss l h f 0 g 0
 
 instance Project AVLBaum Config Config where
     project _ i = i
 
 make :: Make
-make = quiz AVLBaum Baum.Such.Config.example
+make = quiz AVLBaum $ QuizConfig { start_size = 10
+                                 , min_key    = 1
+                                 , max_key    = 99
+                                 , fixed_ops  = 5
+                                 , guess_ops  = 5
+                                 }
