20e2459400de9a50ff0a27f3f852b08ce63fcc37
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-19 14:48:17 +0100

    TypeCheckBonn: trying things out

--- src/TypeCheckBonn/Central.hs
+++ src/TypeCheckBonn/Central.hs
@@ -23,7 +23,7 @@ data TypeCheckBonn = TypeCheckBonn deriving ( Eq, Ord, Show, Read, Typeable )
 instance OrderScore TypeCheckBonn where
     scoringOrder _ = Increasing
 
-newtype ExpBonn = ExpBonn Type.Infer.Exp
+newtype ExpBonn = ExpBonn Type.Infer.Exp deriving ( Typeable )
 
 instance ToDoc  ExpBonn where toDoc (ExpBonn e) = toDoc e
 instance Reader ExpBonn where reader = reader >>= return . ExpBonn
