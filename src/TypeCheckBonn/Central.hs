1b038087afa7c1e476148abfb495e06467733357
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2011-12-19 13:31:39 +0100

    Type.Check -> TypCheckBonn (leaving compatible changes in place)

---
+++ src/TypeCheckBonn/Central.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveDataTypeable, TypeSynonymInstances, MultiParamTypeClasses #-}
+module TypeCheckBonn.Central where
+
+--  $Id$
+
+import Type.Data
+import Type.Tree
+import Type.Infer
+
+import Autolib.Reporter.Type
+import Autolib.ToDoc
+import Autolib.Size
+
+import Autolib.TES.Term
+import Autolib.TES.Position
+import Autolib.TES.Identifier
+
+import Data.Typeable
+import Inter.Types
+import qualified Challenger as C
+
+data TypeCheckBonn = TypeCheckBonn deriving ( Eq, Ord, Show, Read, Typeable )
+
+instance OrderScore TypeCheckBonn where
+    scoringOrder _ = Increasing
+
+instance C.Partial TypeCheckBonn TI Exp where
+
+    describe p i = vcat
+        [ text "Gesucht ist ein Ausdruck vom Typ" <+> toDoc (target i)
+	, text "in der Signatur"
+	, nest 4 $ toDoc (signature i)
+	]
+
+    initial p i = read "f(a,g(b))"
+
+    total p i b = do
+        inform $ vcat 
+	       [ text "Die Baumstruktur dieses Ausdrucks ist"
+	       , nest 4 $ draw b
+	       ]
+	peng b
+        t <- infer (signature i) b
+        assert ( t == target i )
+               $ text "ist das der geforderte Typ?"
+
+instance C.Measure TypeCheckBonn TI Exp where
+    measure p i b = fromIntegral $ size b
+
+make :: Make
+make = direct TypeCheckBonn $
+    TI { target = read "boolean"
+       , signature = read "int a; boolean eq (int a, int b);"
+       }
+
---
+++ src/TypeCheckBonn/Quiz.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FlexibleContexts #-}
+module Type.Quiz where
+
+--  $Id$
+
+import Type.Data
+import TypeCheckBonn.Central
+import Type.Quiz
+
+import Inter.Quiz
+import Inter.Types
+
+
+bonnify :: TI -> TI
+bonnify TI (t sig) = TI t (map discharge sig)
+  where
+    discharge f = f { static = False }
+
+
+instance Generator TypeCheckBonn Conf ( NFTA Int Type, TI ) where
+    generator p conf key = do
+        generator TypeCheck conf key
+
+
+instance Project TypeCheckBonn ( NFTA Int Type, TI ) TI where
+    project p ( au, ti ) = ti
+
+make :: Make
+make = quiz TypeCheckBonn conf
