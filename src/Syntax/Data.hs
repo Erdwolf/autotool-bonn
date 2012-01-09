3d85bc0957f1b5c6787c0aadd6c78a412026c97e
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-01-09 15:28:34 +0100

    Syntax: Configurable generator

--- src/Syntax/Data.hs
+++ src/Syntax/Data.hs
@@ -3,6 +3,7 @@ module Syntax.Data where
 
 import Syntax.Syntax
 import Syntax.Checker
+import Syntax.Generator
 
 import Autolib.ToDoc
 import Autolib.Reader
@@ -12,9 +13,15 @@ import Data.Typeable
 
 deriving instance Ord Graph
 
+deriving instance Eq Frequencies
+deriving instance Ord Frequencies
+deriving instance Eq GeneratorConfig
+deriving instance Ord GeneratorConfig
+
 data QuizConfig = QuizConfig
  { quizFeedback :: Bool
  , seed :: Int
+ , generator :: GeneratorConfig
  }
    deriving ( Eq, Ord, Typeable)
 
@@ -28,7 +35,7 @@ data Config = Config
 data Solution = Solution [String]
    deriving ( Eq, Ord, Typeable, Read, Show )
 
-$(derives [makeReader, makeToDoc] [''QuizConfig,''Config,''Solution,''Graph])
+$(derives [makeReader, makeToDoc] [''QuizConfig,''Config,''Solution,''Graph, ''Frequencies,''GeneratorConfig])
 
 
 instance Size Config   where size _ = 0
