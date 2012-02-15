9f6c4ebcd659a2f6924efafb66cb6f2385f60847
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-02-15 15:12:08 +0100

    ...

--- src/Program/ArrayBonn/Instance.hs
+++ src/Program/ArrayBonn/Instance.hs
@@ -52,8 +52,8 @@ data InstanceConfig = InstanceConfig
 
 instance Generator Program_ArrayBonn Quiz.Config InstanceConfig where
     generator p conf@(Quiz.Config fb _ _ mds _ _ _) = do
-        (i,prog,final) <- R.roll conf `repeat_until` nontrivial
-        return $ InstanceConfig fb i prog final
+        let (i,prog,final) = R.roll conf `repeat_until` nontrivial
+        in return $ InstanceConfig fb i prog final
      where
         nontrivial (i,prog,final) = not $ or $ do
             let bnd = ( 0 , fromIntegral mds )
