a5d591f825225d82e5553e34c615f0bc1fcb33fb
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-03-05 15:14:26 +0100

    ...

--- src/HeapSort/GraphViz.hs
+++ src/HeapSort/GraphViz.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NoMonomorphismRestriction, OverloadedStrings, FlexibleInstances #-}
 module HeapSort.GraphViz (toPng) where
 
-import HeapSort.Tree (toList, Tree)
+import HeapSort.Tree
 
 import System.IO.Unsafe (unsafePerformIO)
 import Text.XHtml (showHtml, Html, image, (!), src, alt, anchor, href)
@@ -26,21 +26,21 @@ picsDir = ".."</>"pics"
 instance ToTree (Data.Tree.Tree String) where
   toTree = id
 
-instance ToTree (T.Tree Int) where
+instance ToTree (Tree Int) where
   toTree = Data.Tree.unfoldTree uf
     where
-      uf (T.Branch x Empty Empty) = (show x,[])
-      uf (T.Branch x l     Empty) = (show x,[l])
-      uf (T.Branch x Empty r    ) = (show x,[r])
-      uf (T.Branch x l     r    ) = (show x,[l,r])
+      uf (Branch x Empty Empty) = (show x,[])
+      uf (Branch x l     Empty) = (show x,[l])
+      uf (Branch x Empty r    ) = (show x,[r])
+      uf (Branch x l     r    ) = (show x,[l,r])
 
-instance ToTree (T.Tree (Marked Int)) where
+instance ToTree (Tree (Marked Int)) where
   toTree = Data.Tree.unfoldTree uf
     where
-      uf (T.Branch x Empty Empty) = (showMarked x,[])
-      uf (T.Branch x l     Empty) = (showMarked x,[l])
-      uf (T.Branch x Empty r    ) = (showMarked x,[r])
-      uf (T.Branch x l     r    ) = (showMarked x,[l,r])
+      uf (Branch x Empty Empty) = (showMarked x,[])
+      uf (Branch x l     Empty) = (showMarked x,[l])
+      uf (Branch x Empty r    ) = (showMarked x,[r])
+      uf (Branch x l     r    ) = (showMarked x,[l,r])
 
       showMarked (Marked   x) = "[" ++ show x ++ "]"
       showMarked (Unmarked x) = show x
