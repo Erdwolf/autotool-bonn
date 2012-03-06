e6afaaa3b26114f4f71c32781a65bdf99bfe4111
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-03-06 16:56:17 +0100

    TextConfig: change in wording

--- src/TextConfig.hs
+++ src/TextConfig.hs
@@ -3,20 +3,20 @@ module TextConfig where
 import Autolib.ToDoc (vcat, text)
 
 ok =
-    vcat [ text "Ja, Ihre Einsendung ist richtig."
+    vcat [ text "Ja, Ihre Einreichung ist richtig."
          , text ""
          , text "Ignorieren Sie die unten angezeigte Bewertung."
          ]
 
 noFeedbackDisclaimer =
     vcat [ text "Hinweis: Bei dieser Aufgabe wird keine Rückmeldung über Korrektheit der Lösung gegeben."
-         , text "         Wenn eine Einsendung akzeptiert wird, heißt dies nicht, dass sie korrekt ist."
+         , text "         Wenn eine Einreichung akzeptiert wird, heißt dies nicht, dass sie korrekt ist."
          ]
 
 noFeedbackResult =
-    vcat [ text "Nicht geprüft."
+    vcat [ text "Verdeckt geprüft."
          , text ""
-         , text "Die Einsendung wird von Ihrem Tutor bewertet."
+         , text "Die Bewertung Ihrer Einreichung erfahren Sie von Ihrem Tutor."
          , text ""
          , text "Ignorieren Sie die unten angezeigte Bewertung."
          ]
