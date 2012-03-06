b34cce856bbccf6cc0ca498872580a78bc7fdf7e
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-03-06 16:51:59 +0100

    TextConfig module contains common text fragments

--- src/AVLBaumBonn/Central.hs
+++ src/AVLBaumBonn/Central.hs
@@ -136,6 +136,7 @@ instance Partial AVLBaum Config OpList where
                      , text "so dass dieser Baum entsteht:"
                      , text ""
                      , text (toPng end)
+                     ]
        when (fb == None) $ do
           inform TextConfig.noFeedbackDisclaimer
        inform $ text "<span style='color:red'>Hinweis: Die zum Rebalancieren des Baumes nötigen <em>Rotationen</em> werden beim Einfügen automatisch durchgeführt. Sie müssen diese <em>nicht</em> mit angeben.</span>"
