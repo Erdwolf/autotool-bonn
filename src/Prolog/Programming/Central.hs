05f23c5d949511c69ab5642f083470d3701c8c24
Matthias Bartsch <bartsch@cs.uni-bonn.de>
2012-03-19 12:29:43 +0100

    ...

--- src/Prolog/Programming/Central.hs
+++ src/Prolog/Programming/Central.hs
@@ -113,7 +113,7 @@ instance Partial Prolog_Programming Config Facts where
                 describe (StatementToCheck query)   = text $ show query
                 describe (Hidden str _)             = text $ "(ein versteckter Test" ++ str ++")"
                 describe (WithTree x)               = describe x
-                describe (WithTreeNegative (StatementToCheck query)) = text $ show [ Struct "not" [t] | t <- query ]
+                describe (WithTreeNegative (StatementToCheck query)) = text $ show [ Struct "not" query ]
             if null incorrect
                then informIO TextConfig.ok
                else rejectIO $ vcat [ text "Nein."
